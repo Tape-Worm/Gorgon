a3b23932e89e659c767c19a18fbe4ef37ba5c765
mwinsor <mwinsor@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-03-10 02:03:45 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@41 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Tools/FileSystemEditor/Properties/AssemblyInfo.cs
+++ Tools/FileSystemEditor/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2990.21619")]
+[assembly: AssemblyFileVersion("1.0.2990.21619")]
