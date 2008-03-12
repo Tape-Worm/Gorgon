94debd2e17be5780e03659812b761e6d7a43c106
Tape-Worm <github@tape-worm.net>
2008-03-12 04:57:37 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@46 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/PlugIns/BMPPlugIn/Properties/AssemblyInfo.cs
+++ Examples/PlugIns/BMPPlugIn/Properties/AssemblyInfo.cs
@@ -60,5 +60,5 @@ using GorgonLibrary.PlugIns;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.2992.38031")]
-[assembly: AssemblyFileVersion("1.0.2992.38031")]
+[assembly: AssemblyVersion("1.0.2992.39432")]
+[assembly: AssemblyFileVersion("1.0.2992.39432")]
