24b09a25533fa6f7cc2b6e0eb8da371ebc932cb0
Tape-Worm <github@tape-worm.net>
2008-03-12 04:10:07 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@44 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/Graphics/ShadersInSpaaace/Properties/AssemblyInfo.cs
+++ Examples/Graphics/ShadersInSpaaace/Properties/AssemblyInfo.cs
@@ -60,5 +60,5 @@ using System.Security.Permissions;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersionAttribute("1.0.2990.21619")]
-[assembly: AssemblyFileVersionAttribute("1.0.2990.21619")]
+[assembly: AssemblyVersion("1.0.2992.38031")]
+[assembly: AssemblyFileVersion("1.0.2992.38031")]
