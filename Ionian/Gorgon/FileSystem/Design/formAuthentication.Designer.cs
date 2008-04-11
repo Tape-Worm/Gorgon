55386450c8273a73c5c973c73fade9d499b232d4
mwinsor <mwinsor@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-04-11 04:40:33 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/Branches/1.x@79 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Ionian/Gorgon/FileSystem/Design/formAuthentication.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace GorgonLibrary.FileSystems.Design
+{
+    partial class formAuthentication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formAuthentication));
+            this.SuspendLayout();
+            // 
+            // formAuthentication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 128);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formAuthentication";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Authentication";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Ionian/Gorgon/FileSystem/Design/formAuthentication.cs
@@ -0,0 +1,72 @@
+﻿#region LGPL.
+// 
+// Gorgon.
+// Copyright (C) 2008 Michael Winsor
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// 
+// Created: Tuesday, April 01, 2008 9:33:41 PM
+// 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GorgonLibrary.FileSystems.Design
+{
+    /// <summary>
+    /// Form used to present authentication to the file system editor.
+    /// </summary>
+    public partial class formAuthentication 
+        : Form
+    {
+        #region Variables.
+        private IAuthData _authorizationData = null;        // Authorization data.
+        #endregion
+
+        #region Properties.
+        /// <summary>
+        /// Property to set or return the authorization data object.
+        /// </summary>
+        [Browsable(false)]
+        public IAuthData AuthorizationData
+        {
+            get
+            {
+                return _authorizationData;
+            }
+            set
+            {
+                _authorizationData = value;
+            }
+        }
+        #endregion
+
+        #region Constructor/Destructor.
+        /// <summary>
+        /// Initializes a new instance of the <see cref="formAuthentication"/> class.
+        /// </summary>
+        public formAuthentication()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
