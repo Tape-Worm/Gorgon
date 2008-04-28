20fbd92c87583cf9e6a3bb0bf9968bbddad6a4f6
Tape-Worm <github@tape-worm.net>
2008-04-28 07:32:24 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@100 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- PlugIns/GorgonBZip2FileSystem/Properties/AssemblyInfo.cs
+++ PlugIns/GorgonBZip2FileSystem/Properties/AssemblyInfo.cs
@@ -56,5 +56,5 @@ using GorgonLibrary.PlugIns;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.3014.31665")]
-[assembly: AssemblyFileVersion("1.0.3014.31665")]
+[assembly: AssemblyVersion("1.0.3040.836")]
+[assembly: AssemblyFileVersion("1.0.3040.836")]
