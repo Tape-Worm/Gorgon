d5e28d3aa331b1ff9e69c2fa5d2e250010e935d0
mwinsor <mwinsor@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-05-01 21:20:40 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/Branches/1.x@113 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Ionian/Gorgon/Utility/ICloneable.cs
@@ -0,0 +1,43 @@
+﻿#region LGPL.
+// 
+// Gorgon.
+// Copyright (C) 2008 Michael Winsor
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+// 
+// Created: Wednesday, April 30, 2008 1:43:23 PM
+// 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GorgonLibrary
+{
+	/// <summary>
+	/// Interface that defines an object that can clone itself.
+	/// </summary>
+	/// <typeparam name="T">Type of the object.</typeparam>
+	/// <remarks>This is like <see cref="System.ICloneable"/> only except that it uses a generic type.  Why this wasn't added in .NET 2.0 I'll never know.</remarks>
+	interface ICloneable<T>
+	{
+		/// <summary>
+		/// Function to create a clone of an instance of an object.
+		/// </summary>
+		/// <returns>A clone of the object.</returns>
+		T Clone();
+	}
+}
