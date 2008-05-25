e09eb89872686da742648fd44c17e1e4d34c03ee
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-05-25 20:12:33 +0000

    Updated version #'s for new release.
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@137 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Gorgon/Properties/AssemblyInfo.cs
+++ Gorgon/Properties/AssemblyInfo.cs
@@ -85,5 +85,5 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.3040.836")]
-[assembly: AssemblyFileVersion("1.0.3040.836")]
+[assembly: AssemblyVersion("1.0.3067.23663")]
+[assembly: AssemblyFileVersion("1.0.3067.23663")]
