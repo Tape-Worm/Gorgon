5d9858b158c3ae46fb6bf64e74d99be2788d3113
Tape-Worm <github@tape-worm.net>
2008-10-03 16:24:14 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@207 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/Framework/DeepAsAPuddle/Properties/AssemblyInfo.cs
+++ Examples/Framework/DeepAsAPuddle/Properties/AssemblyInfo.cs
@@ -64,5 +64,5 @@ using System.Security.Permissions;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3014.31665")]
-[assembly: AssemblyFileVersion("1.0.3014.31665")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
