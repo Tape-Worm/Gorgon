ba437a9c44f5967b1e599f90ab44e64d0279638f
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-11-08 20:46:44 +0000

    version upate
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@247 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/Graphics/TehShadowGn0s/My Project/AssemblyInfo.vb
+++ Examples/Graphics/TehShadowGn0s/My Project/AssemblyInfo.vb
@@ -62,5 +62,5 @@ Imports System.Security.Permissions
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.1.*")> 
-<Assembly: AssemblyFileVersion("1.1.3231.21377")> 
+<Assembly: AssemblyVersion("1.1.3234.23801")> 
+<Assembly: AssemblyFileVersion("1.1.3234.23801")> 
