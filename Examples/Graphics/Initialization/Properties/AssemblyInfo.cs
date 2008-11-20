450822b1e6159523f58f286b2e980a4ecbf469ae
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-11-20 20:54:51 +0000

    Update version #
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@290 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/Graphics/Initialization/Properties/AssemblyInfo.cs
+++ Examples/Graphics/Initialization/Properties/AssemblyInfo.cs
@@ -61,8 +61,8 @@ using System.Security.Permissions;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.3240.32723")]
-[assembly: AssemblyFileVersion("1.1.3240.32723")]
+[assembly: AssemblyVersion("1.1.3246.24958")]
+[assembly: AssemblyFileVersion("1.1.3246.24958")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- Examples/PlugIns/D3D/Properties/AssemblyInfo.cs
+++ Examples/PlugIns/D3D/Properties/AssemblyInfo.cs
@@ -58,5 +58,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3240.32723")]
-[assembly: AssemblyFileVersion("1.1.3240.32723")]
+[assembly: AssemblyVersion("1.1.3246.24958")]
+[assembly: AssemblyFileVersion("1.1.3246.24958")]
