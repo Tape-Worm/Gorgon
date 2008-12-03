1650fe76e5bb51fd600152fda06f9c81d517a211
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-12-03 21:31:17 +0000

    Created a workaround for a bug in older Intel drivers/chipsets.  This bug affects 945 express chipsets and older.
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@300 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- PlugIns/Gorgon3DESFileSystem/Properties/AssemblyInfo.cs
+++ PlugIns/Gorgon3DESFileSystem/Properties/AssemblyInfo.cs
@@ -59,5 +59,5 @@ using GorgonLibrary.PlugIns;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.3258.20122")]
-[assembly: AssemblyFileVersion("1.1.3258.20122")]
+[assembly: AssemblyVersion("1.1.3259.25915")]
+[assembly: AssemblyFileVersion("1.1.3259.25915")]
