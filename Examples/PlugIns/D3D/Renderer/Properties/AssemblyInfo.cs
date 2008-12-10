af0d8d0bab0eb18a99ff00a640d4c2794674d716
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2008-12-10 07:33:05 +0000

    Version Increment.
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@304 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/PlugIns/D3D/Renderer/Properties/AssemblyInfo.cs
+++ Examples/PlugIns/D3D/Renderer/Properties/AssemblyInfo.cs
@@ -60,5 +60,5 @@ using GorgonLibrary.PlugIns;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3259.25915")]
-[assembly: AssemblyFileVersion("1.1.3259.25915")]
+[assembly: AssemblyVersion("1.1.3266.898")]
+[assembly: AssemblyFileVersion("1.1.3266.898")]
