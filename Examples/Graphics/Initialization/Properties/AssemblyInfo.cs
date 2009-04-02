b8686854a6fa382da12d280b6f243ddf48217e04
Tape-Worm <github@tape-worm.net>
2009-04-02 00:35:34 +0000

    Updated to SlimDX March 09.
    Modified image Copy() function to work around bug in Texture2D.FromStream().  Modified image to handle change in FillTexture2D callback.
    
    Modified ShadersInSpaaace example to better show film scratches.
    
    Modified version numbers.
    
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@312 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/Graphics/Initialization/Properties/AssemblyInfo.cs
+++ Examples/Graphics/Initialization/Properties/AssemblyInfo.cs
@@ -61,7 +61,7 @@ using System.Security.Permissions;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.3266.898")]
+[assembly: AssemblyVersion("1.1.*")]
 [assembly: AssemblyFileVersion("1.1.3266.898")]
 
 //
