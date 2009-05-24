a740573a6651fa51135537cc33f2acefdd41ccda
Tape.Wyrm <Tape.Wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2009-05-24 09:04:20 +0000

    Updates for new release (1.1.3425.38459).
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@332 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/PlugIns/D3D/Renderer/Properties/AssemblyInfo.cs
+++ Examples/PlugIns/D3D/Renderer/Properties/AssemblyInfo.cs
@@ -60,5 +60,5 @@ using GorgonLibrary.PlugIns;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1.3266.898")]
+[assembly: AssemblyVersion("1.1.3425.38459")]
+[assembly: AssemblyFileVersion("1.1.3425.38459")]
