f4da761a07ec3a24ea229a8f806c025ace4abd89
Tape-Worm <github@tape-worm.net>
2009-05-30 05:00:37 +0000

    Stupid me, I left a fucking a non-existent vertex declaration in the Batch sprite object.  Batch sprites will fail unless this fix is applied.
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@335 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/Input/INeedYourInput/Properties/AssemblyInfo.cs
+++ Examples/Input/INeedYourInput/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.3425.38459")]
-[assembly: AssemblyFileVersion("1.1.3425.38459")]
+[assembly: AssemblyVersion("1.1.3436.39405")]
+[assembly: AssemblyFileVersion("1.1.3436.39405")]
