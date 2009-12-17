087c1a93e6a210167e14e52babf5c715a375ad9e
Tape-Worm <github@tape-worm.net>
2009-12-17 18:37:08 +0000

    Updating versioning for packaging.
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@339 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/Graphics/Squishy/Properties/AssemblyInfo.cs
+++ Examples/Graphics/Squishy/Properties/AssemblyInfo.cs
@@ -60,8 +60,8 @@ using System.Security.Permissions;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.3436.39405")]
-[assembly: AssemblyFileVersion("1.1.3436.39405")]
+[assembly: AssemblyVersion("1.1.3638.20767")]
+[assembly: AssemblyFileVersion("1.1.3638.20767")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- Examples/PlugIns/PNGPlugIn/Properties/AssemblyInfo.cs
+++ Examples/PlugIns/PNGPlugIn/Properties/AssemblyInfo.cs
@@ -63,5 +63,5 @@ using GorgonLibrary.PlugIns;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.3436.39405")]
-[assembly: AssemblyFileVersion("1.1.3436.39405")]
+[assembly: AssemblyVersion("1.1.3638.20767")]
+[assembly: AssemblyFileVersion("1.1.3638.20767")]
