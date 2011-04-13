8d1c7b62fdd7383b0d43ea8cef8fb03c80093596
Tape-Worm <github@tape-worm.net>
2011-04-13 02:07:50 +0000

    Updating version tags
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@367 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Tools/Atlas/Properties/AssemblyInfo.cs
+++ Tools/Atlas/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.1.3638.20767")]
-[assembly: AssemblyFileVersion("1.1.3638.20767")]
+[assembly: AssemblyFileVersion("1.1.4119.34319")]
