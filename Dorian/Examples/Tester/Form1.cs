4b2dfa9aaa2a948bb23686436306b11be53c8bef
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2011-06-15 14:11:48 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@391 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Examples/Tester/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GorgonLibrary;
+using GorgonLibrary.Graphics;
+
+namespace Tester
+{
+	public partial class Form1 : Form
+	{
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			Gorgon.Initialize();
+
+			Gorgon.SetMode(this);
+
+			Gorgon.Idle += new FrameEventHandler(Gorgon_Idle);
+
+			Gorgon.Go();
+		}
+
+		void Gorgon_Idle(object sender, FrameEventArgs e)
+		{
+			Gorgon.Screen.Clear();
+			Gorgon.Screen.FilledCircle(Gorgon.Screen.Width / 2, Gorgon.Screen.Height / 2, 25.0f, Color.Black);
+		}
+
+		protected override void OnFormClosing(FormClosingEventArgs e)
+		{
+			base.OnFormClosing(e);
+
+			Gorgon.Terminate();
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
