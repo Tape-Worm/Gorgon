fe2c5dfbd2a403bf438bf675840fe03333828193
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2011-06-23 22:24:56 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@406 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/TesterPlugIn/EntryPoint.cs
+++ Dorian/Examples/TesterPlugIn/EntryPoint.cs
@@ -8,6 +8,14 @@ namespace TesterPlugIn
 	public class EntryPoint
 		: GorgonLibrary.PlugIns.GorgonPlugIn
 	{
-		
+		public void ShowShit()
+		{
+			System.Windows.Forms.MessageBox.Show("Test");
+		}
+
+		public EntryPoint()
+			: base("Test plug-in.")
+		{
+		}
 	}
 }
