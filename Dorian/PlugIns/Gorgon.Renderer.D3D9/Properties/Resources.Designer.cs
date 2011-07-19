ecba038f0993db7236c2dedd19f24cdfc5c51da1
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2011-07-19 23:11:41 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@500 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/PlugIns/Gorgon.Renderer.D3D9/Properties/Resources.Designer.cs
+++ Dorian/PlugIns/Gorgon.Renderer.D3D9/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GorgonLibrary.Renderer.D3D9.Properties {
+namespace GorgonLibrary.Graphics.D3D9Renderer.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GorgonLibrary.Renderer.D3D9.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GorgonLibrary.Renderer.D3D9.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GorgonLibrary.Graphics.D3D9Renderer.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
