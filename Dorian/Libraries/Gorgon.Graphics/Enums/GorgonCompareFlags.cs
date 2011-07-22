25092a167c79e45fe40abd9ae17557d6577cd19a
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2011-07-22 22:35:20 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@512 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Libraries/Gorgon.Graphics/Enums/GorgonCompareFlags.cs
@@ -0,0 +1,73 @@
+﻿#region MIT.
+// 
+// Gorgon.
+// Copyright (C) 2011 Michael Winsor
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// Created: Friday, July 22, 2011 3:24:42 PM
+// 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GorgonLibrary.Graphics
+{
+	/// <summary>
+	/// Comparison operation.
+	/// </summary>
+	[Flags()]
+	public enum GorgonCompareFlags
+	{
+		/// <summary>
+		/// Never allow the value.
+		/// </summary>
+		Never = 1,
+		/// <summary>
+		/// Allow if the value is less than.
+		/// </summary>
+		Less = 2,
+		/// <summary>
+		/// Allow if the value is equal.
+		/// </summary>
+		Equal = 4,
+		/// <summary>
+		/// Allow if the value is less than or equal.
+		/// </summary>
+		LessEqual = 8,
+		/// <summary>
+		/// Allow if the value is greater than.
+		/// </summary>
+		Greater = 16,
+		/// <summary>
+		/// Allow if the values are not equal.
+		/// </summary>
+		NotEqual = 32,
+		/// <summary>
+		/// Allow if the value is greater than.
+		/// </summary>
+		GreaterEqual = 64,
+		/// <summary>
+		/// Always allow the value.
+		/// </summary>
+		Always = 128
+	}
+}
--- Dorian/PlugIns/Gorgon.Graphics.D3D9/DeviceInfo/D3D9VideoDevice.cs
+++ Dorian/PlugIns/Gorgon.Graphics.D3D9/DeviceInfo/D3D9VideoDevice.cs
@@ -28,6 +28,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
+using SlimDX.Direct3D9;
 
 namespace GorgonLibrary.Graphics.D3D9
 {
@@ -38,18 +40,41 @@ namespace GorgonLibrary.Graphics.D3D9
 		: GorgonVideoDevice
 	{
 		#region Variables.
-
+		private AdapterInformation _adapter = null;		// Adapter information.
+		private Capabilities _caps = null;				// Adapter capabilities.
 		#endregion
 
 		#region Properties.
 		/// <summary>
-		/// Property to return a description of the device.
+		/// Property to return the D3D9 adapter index.
 		/// </summary>
-		public override string Description
+		public int AdapterIndex
 		{
 			get
 			{
-				return "Fill me in.";
+				return _caps.AdapterOrdinal;
+			}
+		}
+
+		/// <summary>
+		/// Property to return the index of the adapter head for the device.
+		/// </summary>
+		public int Head
+		{
+			get
+			{
+				return _caps.AdapterOrdinalInGroup;
+			}
+		}
+
+		/// <summary>
+		/// Property to return whether the device is hardware accelerated.
+		/// </summary>
+		public bool HWAccelerated
+		{
+			get
+			{
+				return (_caps.DeviceCaps & DeviceCaps.HWRasterization) == DeviceCaps.HWRasterization;
 			}
 		}
 		#endregion
@@ -63,9 +88,54 @@ namespace GorgonLibrary.Graphics.D3D9
 		/// </returns>
 		protected override IEnumerable<KeyValuePair<string, string>> GetDeviceCapabilities()
 		{
-			IDictionary<string, string> _deviceCaps = null;
+			IDictionary<string, string> deviceCaps = null;
 
-			return _deviceCaps;			
+			deviceCaps = new SortedList<string, string>();
+
+			VertexShaderVersion = _caps.VertexShaderVersion;
+			PixelShaderVersion = _caps.PixelShaderVersion;
+						
+			deviceCaps.Add("D3D_AdapterIndex", _caps.AdapterOrdinal.ToString());
+			deviceCaps.Add("D3D_HeadIndex", _caps.AdapterOrdinalInGroup.ToString());
+			deviceCaps.Add("D3D_DeviceID", "0x" + GorgonUtility.FormatHex(_adapter.Details.DeviceId));
+			deviceCaps.Add("D3D_DeviceName", _adapter.Details.DeviceName.ToString());
+			deviceCaps.Add("D3D_DriverName", _adapter.Details.DriverName.ToString());
+			deviceCaps.Add("D3D_DriverVersion", _adapter.Details.DriverVersion.ToString());
+			deviceCaps.Add("D3D_Revision", _adapter.Details.Revision.ToString());
+			deviceCaps.Add("D3D_SubSystemID", "0x" + GorgonUtility.FormatHex(_adapter.Details.SubsystemId));
+			deviceCaps.Add("D3D_VendorID", "0x" + GorgonUtility.FormatHex(_adapter.Details.VendorId));
+			deviceCaps.Add("D3D_GUID", _adapter.Details.DeviceIdentifier.ToString());
+
+			var caps = _caps.GetType().GetProperties().Where(item => !item.Name.StartsWith("AdapterOrdinal"));
+
+			// Extract device capabilities.
+			foreach (var cap in caps)
+			{
+				if ((cap.PropertyType.IsEnum) || (cap.PropertyType == typeof(string)) || (cap.PropertyType.IsPrimitive) || (cap.PropertyType == typeof(Version)))
+					deviceCaps.Add("D3D9Cap_" + cap.Name, cap.GetValue(_caps, null).ToString());
+				else
+				{
+					if (cap.PropertyType.IsValueType)
+					{
+						object value = cap.GetValue(_caps, null);
+						var subCap = value.GetType().GetProperties();
+
+						foreach (var sub in subCap)
+							deviceCaps.Add("D3D9Cap_" + cap.Name + "." + sub.Name, sub.GetValue(value, null).ToString());
+					}
+				}
+			}
+
+			var capInfo = deviceCaps.Where(item => item.Key.StartsWith("D3D_"));
+			Gorgon.Log.Print("Device Info:", Diagnostics.GorgonLoggingLevel.Intermediate);
+			foreach (var item in capInfo)
+				Gorgon.Log.Print("\t{0}: {1}", Diagnostics.GorgonLoggingLevel.Intermediate, item.Key, item.Value);
+
+			capInfo = deviceCaps.Where(item => item.Key.StartsWith("D3D9Cap_"));
+			Gorgon.Log.Print("Device Capabilities:", Diagnostics.GorgonLoggingLevel.Verbose);
+			foreach (var item in capInfo)
+				Gorgon.Log.Print("\t{0}: {1}", Diagnostics.GorgonLoggingLevel.Verbose, item.Key, item.Value);
+			return deviceCaps;			
 		}
 		#endregion
 
@@ -73,11 +143,22 @@ namespace GorgonLibrary.Graphics.D3D9
 		/// <summary>
 		/// Initializes a new instance of the <see cref="D3D9VideoDevice"/> class.
 		/// </summary>
+		/// <param name="adapter">D3D9 adapter.</param>
+		/// <param name="capabilities">Adapter capabilities.</param>
 		/// <param name="index">Index of the driver in the collection.</param>
-		internal D3D9VideoDevice(int index)
+		internal D3D9VideoDevice(AdapterInformation adapter, Capabilities capabilities, int index)
 			: base(index)
 		{
-			
+			if (adapter == null)
+				throw new ArgumentNullException("adapter");
+			if (capabilities == null)
+				throw new ArgumentNullException("capabilities");
+
+			_adapter = adapter;
+			_caps = capabilities;
+			Name = _adapter.Details.Description;			
+
+			Gorgon.Log.Print("Video Device #{0}: {1}", Diagnostics.GorgonLoggingLevel.Simple, index, adapter.Details.Description);
 		}
 		#endregion
 	}
