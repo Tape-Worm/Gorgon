6f4140518efaf02f2435a5299217ddc50acc2619
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2011-10-12 20:48:36 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@673 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Libraries/Gorgon.Graphics/IObjectTracker.cs
@@ -0,0 +1,62 @@
+﻿#region MIT.
+// 
+// Gorgon.
+// Copyright (C) 2011 Michael Winsor
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// Created: Thursday, August 04, 2011 8:33:00 AM
+// 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GorgonLibrary.Graphics
+{
+	/// <summary>
+	/// An interface used to define objects that do their own clean up of child objects.
+	/// </summary>
+	interface IObjectTracker
+	{
+		#region Properties.
+		/// <summary>
+		/// Property to return an enumerable list of tracked objects.
+		/// </summary>
+		IEnumerable<IDisposable> TrackedObjects
+		{
+			get;
+		}
+		#endregion
+
+		#region Methods.
+		/// <summary>
+		/// Function to remove a tracked object from the list.
+		/// </summary>
+		/// <param name="trackedObject">The tracked object to remove.</param>
+		void RemoveTrackedObject(IDisposable trackedObject);
+
+		/// <summary>
+		/// Function to clean up any objects being tracked.
+		/// </summary>
+		void CleanUpTrackedObjects();
+		#endregion
+	}
+}
