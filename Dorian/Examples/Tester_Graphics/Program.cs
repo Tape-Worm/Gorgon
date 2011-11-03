880c49d4e9be224b7e8f0684fd7b4db83479cf62
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2011-11-03 01:05:36 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@708 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Tester_Graphics/Program.cs
+++ Dorian/Examples/Tester_Graphics/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using GorgonLibrary;
 
 namespace Tester_Graphics
 {
@@ -15,7 +16,8 @@ namespace Tester_Graphics
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+
+			Gorgon.Run(new Form1());
 		}
 	}
 }
