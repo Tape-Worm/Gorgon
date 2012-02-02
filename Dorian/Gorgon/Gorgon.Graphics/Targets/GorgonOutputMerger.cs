ff3fd567ce7dd6c53cf4ef9c5ae4accfcb18d10e
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2012-02-02 14:45:27 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@810 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Gorgon/Gorgon.Graphics/Targets/GorgonOutputMerger.cs
@@ -0,0 +1,169 @@
+﻿#region MIT.
+// 
+// Gorgon.
+// Copyright (C) 2012 Michael Winsor
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// Created: Tuesday, January 31, 2012 12:38:41 PM
+// 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GorgonLibrary.Diagnostics;
+
+namespace GorgonLibrary.Graphics
+{
+	/// <summary>
+	/// Manages the display of the graphics data.
+	/// </summary>
+	public sealed class GorgonOutputMerger
+	{
+		#region Variables.
+		private GorgonGraphics _graphics = null;
+		#endregion
+
+		#region Properties.
+		/// <summary>
+		/// Property to return the blending render state interface.
+		/// </summary>
+		public GorgonBlendRenderState BlendingState
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Property to return the depth/stencil render state interface.
+		/// </summary>
+		public GorgonDepthStencilRenderState DepthStencilState
+		{
+			get;
+			private set;
+		}
+		#endregion
+
+		#region Methods.
+		/// <summary>
+		/// Function to clean up resources used by the interface.
+		/// </summary>
+		internal void CleanUp()
+		{
+			if (BlendingState != null)
+				((IDisposable)BlendingState).Dispose();
+			if (DepthStencilState != null)
+				((IDisposable)DepthStencilState).Dispose();
+
+			BlendingState = null;
+			DepthStencilState = null;
+		}
+
+		/// <summary>
+		/// Function to create a depth/stencil buffer.
+		/// </summary>
+		/// <param name="name">Name of the depth/stencil buffer.</param>
+		/// <param name="settings">Settings to apply to the depth/stencil buffer.</param>
+		/// <returns>A new depth/stencil buffer.</returns>
+		/// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="name"/> parameter is NULL (Nothing in VB.Net).
+		/// <para>-or-</para>
+		/// <para>Thrown when the <paramref name="settings"/> parameter is NULL (Nothing in VB.Net).</para>
+		/// </exception>
+		/// <exception cref="System.ArgumentException">Thrown when the name parameter is an empty string.
+		/// <para>-or-</para>
+		/// <para>Thrown when the <see cref="P:GorgonLibrary.Graphics.GorgonDepthStencilSettings.Format">GorgonDepthStencilSettings.Format</see> property is set to Unknown or is unsupported.</para>
+		/// </exception>
+		/// <remarks>
+		/// A depth buffer may be paired with a swapchain or render target through its DepthStencil property.  When pairing the depth/stencil to the render target, Ensure that the depth/stencil buffer width, height and multisample settings match that of the render target that it is paired with.
+		/// <para>The texture for a depth/stencil may be used in a shader for cards that have a feature level of SM_4_1 or better, and can be set to do so by setting the <see cref="P:GorgonLibrary.Graphics.GorgonDepthStencilSettings.TextureFormat">GorgonDepthStencilSettings.TextureFormat</see> property to a typeless format. 
+		/// If this is attempted on a video device that has a feature level of SM_4_0 or below, then an exception will be raised.</para>
+		/// </remarks>
+		public GorgonDepthStencil CreateDepthStencil(string name, GorgonDepthStencilSettings settings)
+		{
+			GorgonDepthStencil depthBuffer = null;
+
+			GorgonDebug.AssertNull<GorgonDepthStencilSettings>(settings, "settings");
+
+			GorgonDepthStencil.ValidateSettings(_graphics, settings);
+
+			depthBuffer = new GorgonDepthStencil(_graphics, name, settings);
+			_graphics.TrackedObjects.Add(depthBuffer);
+			depthBuffer.UpdateSettings();
+
+			return depthBuffer;
+		}
+
+		/// <summary>
+		/// Function to create a swap chain.
+		/// </summary>
+		/// <param name="name">Name of the swap chain.</param>
+		/// <param name="settings">Settings for the swap chain.</param>
+		/// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="name"/> parameter is NULL (Nothing in VB.Net).
+		/// <para>-or-</para>
+		/// <para>Thrown when the <paramref name="settings"/> parameter is NULL (Nothing in VB.Net).</para>
+		/// </exception>
+		/// <exception cref="System.ArgumentException">Thrown when the name parameter is an empty string.
+		/// <para>-or-</para>
+		/// <para>Thrown when the <see cref="P:GorgonLibrary.Graphics.GorgonSwapChainSettings.Window">GorgonSwapChainSettings.Window</see> property is NULL (Nothing in VB.Net), and the <see cref="P:GorgonLibrary.Gorgon.ApplicationForm">Gorgon application window</see> is NULL.</para>
+		/// <para>-or-</para>
+		/// <para>Thrown when the <see cref="P:GorgonLibrary.Graphics.GorgonVideoMode.Format">GorgonSwapChainSettings.VideoMode.Format</see> property cannot be used by the video device for displaying data.</para>
+		/// <para>-or-</para>
+		/// <para>Thrown when the <see cref="P:GorgonLibrary.Graphics.GorgonSwapChainSettings.MultSamples.Quality">GorgonSwapChainSettings.MultiSamples.Quality</see> property is less than 0 or not less than the value returned by <see cref="M:GorgonLibrary.Graphics.GorgonVideoDevice">GorgonVideoDevice.GetMultiSampleQuality</see>.</para>
+		/// </exception>
+		/// <exception cref="GorgonLibrary.GorgonException">Thrown when the video output could not be determined from the window.
+		/// <para>-or-</para>
+		/// <para>Thrown when the swap chain is going to full screen mode and another swap chain is already on the video output.</para>
+		/// </exception>
+		/// <remarks>This will create our output swap chains for display to a window or control.  All functionality for sending or retrieving data from the video device can be accessed through the swap chain.
+		/// <para>Passing default settings for the <see cref="GorgonLibrary.Graphics.GorgonSwapChainSettings">settings parameters</see> will make Gorgon choose the closest possible settings appropriate for the video device and output that the window is on.  For example, passing NULL (Nothing in VB.Net) to 
+		/// the <see cref="P:GorgonLibrary.Graphics.GorgonSwapChainSettings.VideoMode">GorgonSwapChainSettings.VideoMode</see> parameter will make Gorgon find the closest video mode available to the current window size and desktop format (for the output).</para>
+		/// <para>If the multisampling quality in the <see cref="P:GorgonLibrary.Graphics.GorgonSwapChainSettings.MultSamples.Quality">GorgonSwapChainSettings.MultiSamples.Quality</see> property is higher than what the video device can support, an exception will be raised.  To determine 
+		/// what the maximum quality for the sample count for the video device should be, call the <see cref="M:GorgonLibrary.Graphics.GorgonVideoDevice.GetMultiSampleQuality">GorgonVideoDevice.GetMultiSampleQuality</see> method.</para>
+		/// </remarks>
+		public GorgonSwapChain CreateSwapChain(string name, GorgonSwapChainSettings settings)
+		{
+			GorgonSwapChain swapChain = null;
+
+			GorgonDebug.AssertNull<GorgonSwapChainSettings>(settings, "settings");
+
+			GorgonSwapChain.ValidateSwapChainSettings(_graphics, settings);
+
+			swapChain = new GorgonSwapChain(_graphics, name, settings);
+			_graphics.TrackedObjects.Add(swapChain);
+			swapChain.Initialize();
+
+			return swapChain;
+		}
+		#endregion
+
+		#region Constructor/Destructor.
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GorgonOutputMerger"/> class.
+		/// </summary>
+		/// <param name="graphics">The graphics.</param>
+		internal GorgonOutputMerger(GorgonGraphics graphics)
+		{
+			_graphics = graphics;
+			BlendingState = new GorgonBlendRenderState(_graphics);
+			DepthStencilState = new GorgonDepthStencilRenderState(_graphics);
+		}
+		#endregion
+	}
+}
