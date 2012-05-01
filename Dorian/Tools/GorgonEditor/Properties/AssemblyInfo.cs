87f4916ea653e17abc0bad5a1e28d69c8f86da59
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2012-05-01 00:20:09 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@957 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Tools/GorgonEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gorgon Editor")]
+[assembly: AssemblyDescription("An application to create/edit various items (Sprites, Fonts, etc...) for the Gorgon components.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michael Winsor")]
+[assembly: AssemblyProduct("GorgonEditor")]
+[assembly: AssemblyCopyright("Copyright © Michael Winsor 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e9e50b7f-823a-4df6-960d-a8d3f8c31f89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
