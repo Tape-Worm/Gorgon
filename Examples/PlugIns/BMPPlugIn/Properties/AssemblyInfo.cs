9ee492106735c06e54c302015eaa5581ed889760
tape.wyrm@gmail.com <tape.wyrm@gmail.com@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2012-05-27 17:28:36 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@1017 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/PlugIns/BMPPlugIn/Properties/AssemblyInfo.cs
+++ Examples/PlugIns/BMPPlugIn/Properties/AssemblyInfo.cs
@@ -63,5 +63,5 @@ using GorgonLibrary.PlugIns;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.1.4529.31450")]
 [assembly: AssemblyFileVersion("1.1.4529.31450")]
