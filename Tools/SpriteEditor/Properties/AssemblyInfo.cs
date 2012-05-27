6a6c77223dca3e9fcf9d0b7e22a5ae0463003e32
tape.wyrm@gmail.com <tape.wyrm@gmail.com@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2012-05-27 17:28:58 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@1018 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Tools/SpriteEditor/Properties/AssemblyInfo.cs
+++ Tools/SpriteEditor/Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.1.4529.31450")]
 [assembly: AssemblyFileVersion("1.1.4529.31450")]
