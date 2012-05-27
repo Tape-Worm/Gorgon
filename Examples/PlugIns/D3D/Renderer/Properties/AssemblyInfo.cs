1e097b8495e03f921edbacb0bee8245e356b5a43
Tape-Worm <github@tape-worm.net>
2012-05-27 17:25:20 +0000

    Resetting versioning.
    git-svn-id: https://gorgonlib.googlecode.com/svn/trunk@1014 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Examples/PlugIns/D3D/Renderer/Properties/AssemblyInfo.cs
+++ Examples/PlugIns/D3D/Renderer/Properties/AssemblyInfo.cs
@@ -60,5 +60,5 @@ using GorgonLibrary.PlugIns;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.4529.31450")]
+[assembly: AssemblyVersion("1.1.*")]
 [assembly: AssemblyFileVersion("1.1.4529.31450")]
