7b915030941a2d4b3a4685750f709f22aa3345eb
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2012-06-05 15:07:17 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1029 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Tester_Graphics/Program.cs
+++ Dorian/Examples/Tester_Graphics/Program.cs
@@ -26,24 +26,6 @@ namespace Tester_Graphics
 			//GorgonDialogs.ConfirmBox(null, "Fuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=Fuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=Fuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=", true, false);
 			//GorgonDialogs.ConfirmBox(null, "Fuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=Fuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=Fuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=\nFuck you ABCDEFGHIJKLMNOPQSTUVWXYZ 1234567890 !@#$%^&*()_+-=", true, true);
 
-			GorgonFileSystem fs = new GorgonFileSystem();
-			GorgonFileSystemSaveFile dialog = null;
-
-			fs.Mount(@"d:\unpak\");
-			fs.WriteLocation = @"d:\unpak\";
-
-			dialog = new GorgonFileSystemSaveFile(fs, "/");
-			//dialog.InitialDirectory = "/mono-csvorbis-2ae35b3";
-			//dialog.DefaultExtension = "zip";
-			//dialog.Filter = "WinZip files (*.zip)|*.zip;*.zeep|All the files (*.*)|*.*";
-			//dialog.FilterIndex = 1;
-			dialog.View = FileDialogView.Icons;
-			if (dialog.ShowDialog() == DialogResult.OK)
-			{
-				GorgonDialogs.InfoBox(null, string.Join("\n", dialog.Filenames));
-			}
-
-			return;
 			Gorgon.Run(new Form1());
 		}
 	}
