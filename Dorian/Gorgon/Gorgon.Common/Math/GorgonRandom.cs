008c1289239d2020866bc0d2fae2a319b979cb07
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2012-10-15 04:34:38 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1099 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Gorgon/Gorgon.Common/Math/GorgonRandom.cs
+++ Dorian/Gorgon/Gorgon.Common/Math/GorgonRandom.cs
@@ -28,6 +28,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SlimMath;
 
 namespace GorgonLibrary.Math
 {
@@ -37,6 +38,8 @@ namespace GorgonLibrary.Math
 	public static class GorgonRandom
 	{
 		#region Variables.
+		private static Vector3[] _gradients = null;			// Gradients for Perlin noise.
+		private static int[] _permutations = null;			// Perlin noise permutations for gradients.
 		private static int _seed;
 		private static Random _rnd = null;
 		#endregion
@@ -55,21 +58,149 @@ namespace GorgonLibrary.Math
 			{
 				_seed = value;
 				_rnd = new Random(_seed);
+				CreatePerlinGradients();
 			}
 		}
 		#endregion
 
 		#region Methods.
 		/// <summary>
+		/// Function to create the gradient values for the Perlin noise.
+		/// </summary>
+		private static void CreatePerlinGradients()
+		{
+			var values = Enumerable.Range(0, 256).ToList();
+			_permutations = Enumerable.Repeat<int>(-1, 256).ToArray();
+			_gradients = new Vector3[256];
+
+			// Compute gradients and shuffle permutations.
+			for (int i = 0; i < 256; i++)
+			{
+				float value = RandomSingle() * 2.0f - 1.0f;
+				float range = (1.0f - (value * value)).Sqrt();
+				float theta = ((float)System.Math.PI) * 2.0f * RandomSingle();
+				int valueIndex = RandomInt32(values.Count);
+
+				_permutations[i] = values[valueIndex];
+				values.RemoveAt(valueIndex);
+
+				_gradients[i].X = range * theta.Cos();
+				_gradients[i].Y = range * theta.Sin();
+				_gradients[i].Z = value;
+			}
+		}
+
+		/// <summary>
+		/// Function to look up the random gradient value at the specified coordinates.
+		/// </summary>
+		/// <param name="x">Horizontal position.</param>
+		/// <param name="y">Vertical position.</param>
+		/// <param name="z">Depth position.</param>
+		/// <param name="value">Value to dot with the index value.</param>
+		/// <returns>The randomized value.</returns>
+		private static float PerlinLattice3(int x, int y, int z, Vector3 value)
+		{
+			float result = 0.0f;
+			int index = _permutations[(_permutations[(_permutations[z & 0xFF] + y) & 0xFF] + x) & 0xFF];
+
+			Vector3.Dot(ref _gradients[index], ref value, out result);
+
+			return result;
+		}
+
+		/// <summary>
+		/// Function to look up the random gradient value at the specified coordinates.
+		/// </summary>
+		/// <param name="x">Horizontal position.</param>
+		/// <param name="y">Vertical position.</param>
+		/// <param name="value">Value to dot with the index value.</param>
+		/// <returns>The randomized value.</returns>
+		private static float PerlinLattice2(int x, int y, Vector2 value)
+		{
+			float result = 0.0f;
+			int index = _permutations[(_permutations[y & 0xFF] + x) & 0xFF];
+
+			Vector2 gradient = (Vector2)_gradients[index];
+			Vector2.Dot(ref gradient, ref value, out result);
+
+			return result;
+		}
+
+
+		/// <summary>
+		/// Function to retrieve a 2 dimensional Perlin noise value.
+		/// </summary>
+		/// <param name="source">The source value to use to generate the Perline noise value.</param>
+		/// <returns>The Perlin noise value.</returns>
+		public static float Perlin(Vector2 source)
+		{
+			int x = (int)System.Math.Floor(source.X);
+			int y = (int)System.Math.Floor(source.Y);
+			Vector2 diff = new Vector2(x - source.X, y - source.Y);
+			Vector2 neg = new Vector2(diff.X - 1.0f, diff.Y - 1.0f);
+			Vector2 smoothed = new Vector2(diff.X * diff.X * (3 - 2 * diff.X), diff.Y * diff.Y * (3 - 2 * diff.Y));
+
+			Vector2 value1 = Vector2.Zero;
+			Vector2 value2 = Vector2.Zero;
+
+			value1.X = PerlinLattice2(x, y, diff);
+			value2.X = PerlinLattice2(x + 1, y, new Vector2(neg.X, diff.Y));
+			value1.Y = value1.X + (value2.X - value1.X) * smoothed.X;
+
+			value1.X = PerlinLattice2(x, y + 1, new Vector2(diff.X, neg.Y));
+			value2.X = PerlinLattice2(x + 1, y + 1, new Vector2(neg.X, neg.Y));
+			value2.Y = value1.X + (value2.X - value1.X) * smoothed.X;
+
+			return value1.Y + (value2.Y - value1.Y) * smoothed.Y;
+		}
+
+		/// <summary>
+		/// Function to retrieve a 3 dimensional Perlin noise value.
+		/// </summary>
+		/// <param name="source">The source value to use to generate the Perline noise value.</param>
+		/// <returns>The Perlin noise value.</returns>
+		public static float Perlin(Vector3 source)
+		{
+			int x = (int)System.Math.Floor(source.X);
+			int y = (int)System.Math.Floor(source.Y);
+			int z = (int)System.Math.Floor(source.Z);
+			Vector3 diff = new Vector3(x - source.X, y - source.Y, z - source.Z);
+			Vector3 neg = new Vector3(diff.X - 1.0f, diff.Y - 1.0f, diff.Z - 1.0f);
+			Vector3 smoothed = new Vector3(diff.X * diff.X * (3 - 2 * diff.X), diff.Y * diff.Y * (3 - 2 * diff.Y), diff.Z * diff.Z * (3 - 2 * diff.Z));
+
+			Vector3 value1 = Vector3.Zero;
+			Vector3 value2 = Vector3.Zero;
+
+			value1.X = PerlinLattice3(x, y, z, diff);
+			value2.X = PerlinLattice3(x + 1, y, z, new Vector3(neg.X, diff.Y, diff.Z));
+			value1.Y = value1.X + (value2.X - value1.X) * smoothed.X;
+
+			value1.X = PerlinLattice3(x, y + 1, z, new Vector3(diff.X, neg.Y, diff.Z));
+			value2.X = PerlinLattice3(x + 1, y + 1, z, new Vector3(neg.X, neg.Y, diff.Z));
+			value2.Y = value1.X + (value2.X - value1.X) * smoothed.X;
+			value1.Z = value1.Y + (value2.Y - value1.Y) * smoothed.Y;
+
+			value1.X = PerlinLattice3(x, y, z + 1, new Vector3(diff.X, diff.Y, neg.Z));
+			value2.X = PerlinLattice3(x + 1, y, z + 1, new Vector3(neg.X, diff.Y, neg.Z));
+			value1.Y = value1.X + (value2.X - value1.X) * smoothed.X;
+
+			value1.X = PerlinLattice3(x, y + 1, z + 1, new Vector3(diff.X, neg.Y, neg.Z));
+			value2.X = PerlinLattice3(x + 1, y + 1, z + 1, neg);
+			value2.Y = value1.X + (value2.X - value1.X) * smoothed.X;
+
+			value2.Z = value1.Y + (value2.Y - value1.Y) * smoothed.Y;
+
+			return value1.Z + (value2.Z - value1.Z) * smoothed.Z;
+		}
+
+		/// <summary>
 		/// Function to return a random floating point number.
 		/// </summary>
 		/// <param name="start">Starting value for the random number.</param>
-		/// <param name="end">Ending value for the random number range.  This value is not inclusive.</param>
+		/// <param name="end">Ending value for the random number range.  This value is inclusive.</param>
 		/// <returns>The random number from <paramref name="start"/> to <paramref name="end"/>.</returns>
 		public static float RandomSingle(float start, float end)
 		{
-			end -= 0.000001f;
-
 			if (start < end)
 				return (float)_rnd.NextDouble() * (end - start) + start;
 			else
@@ -79,7 +210,7 @@ namespace GorgonLibrary.Math
 		/// <summary>
 		/// Function to return a random floating point number.
 		/// </summary>
-		/// <param name="maxValue">The highest number for random values, this value is not inclusive.</param>
+		/// <param name="maxValue">The highest number for random values, this value is inclusive.</param>
 		/// <returns>A random number</returns>.
 		public static float RandomSingle(float maxValue)
 		{
@@ -87,9 +218,9 @@ namespace GorgonLibrary.Math
 		}
 
 		/// <summary>
-		/// Function to return a random floating point number.
+		/// Function to return a random floating point number between 0 and 1.0f.
 		/// </summary>
-		/// <returns>A random number</returns>
+		/// <returns>A random number between 0 and 1.</returns>
 		public static float RandomSingle()
 		{
 			return (float)_rnd.NextDouble();
