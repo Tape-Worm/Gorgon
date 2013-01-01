7e264a9fbbfba7131ccb526d764985c61764ec24
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-01-01 20:08:09 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1117 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Gorgon.Graphics/Boinger/Properties/Settings.Designer.cs
+++ Dorian/Examples/Gorgon.Graphics/Boinger/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace GorgonLibrary.Graphics.Example.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1920, 1200")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1280, 800")]
         public global::System.Drawing.Size Resolution {
             get {
                 return ((global::System.Drawing.Size)(this["Resolution"]));
@@ -34,7 +34,7 @@ namespace GorgonLibrary.Graphics.Example.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool IsWindowed {
             get {
                 return ((bool)(this["IsWindowed"]));
