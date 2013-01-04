087c2e944eea81fc99f27b90bf3a14f59e037a13
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-01-04 15:55:13 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1128 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Gorgon.Input/InputFactory/Program.cs
+++ Dorian/Examples/Gorgon.Input/InputFactory/Program.cs
@@ -41,6 +41,27 @@ namespace GorgonLibrary.Examples
 	/// Entry point class.
 	/// </summary>
 	/// <remarks>
+    /// The Input system in this version of Gorgon is very different from the previous version.  Input plug-ins now create factories which
+    /// will create input device objects (e.g. a mouse device, keyboard device, etc...).  This allows mixing and matching of various input
+    /// types.  For example, you can load a winforms plug-in to handle keyboard input, a raw input plug-in to handle mouse input and a
+    /// plug-in to handle XBox controller support.  In fact, the XBox controller plug-in only has support for the controller and has no
+    /// mouse or keyboard device support.
+    /// 
+    /// The first step in setting up the input interface is to create an input factory.  In this example we'll load the plug-ins and create 
+    /// their respective input factories.  This example will go to the plug-ins directory (which can be configured in the config file) and
+    /// load all of the input plug-ins by enumerating the DLLs and checking for input plug-in.  It is important to note that this is done 
+    /// for the example only, in most cases the developer will know the DLL and plug-in type and create it directly.  For example:
+    /// 
+    /// // Load the raw input plug-in DLL.
+    /// var assemblyName = Gorgon.PlugIns.LoadPlugInAssembly("[directory for plug-ins]\Gorgon.Input.Raw.DLL");
+    ///
+    /// // Create the factory.  Notice that we're using the full type name of the plug-in.
+    /// // Alternatively, one could enumerate the plug-ins after loading the assembly by calling:
+    /// // var plugIns = Gorgon.PlugIns.EnumeratePlugIns(assemblyName);
+    /// // And then check for a GorgonInputPlugIn in the list returned.
+    /// GorgonInputFactory rawInput = GorgonInputFactory.CreateInputFactory("GorgonLibrary.Input.GorgonRawPlugIn");
+    /// 
+    /// While this is more complex than the previous version of Gorgon, it's also far more flexible.
 	/// </remarks>
 	static class Program
 	{
@@ -80,8 +101,14 @@ namespace GorgonLibrary.Examples
 		/// </summary>
 		/// <returns>A list of input plug-ins.</returns>
 		static IList<Tuple<GorgonInputPlugIn, GorgonInputFactory>> GetInputPlugIns()
-		{
-			IList<Tuple<GorgonInputPlugIn, GorgonInputFactory>> result = new Tuple<GorgonInputPlugIn, GorgonInputFactory>[] { };
+		{            
+			IList<Tuple<GorgonInputPlugIn, GorgonInputFactory>> result = new Tuple<GorgonInputPlugIn, GorgonInputFactory>[] { };    // A list of input factories and the plug-ins that created them.
+
+            // Get the files from the plug-in directory.
+            // The plug-in directory can be changed in the configuration file
+            // to point at wherever you'd like.  If a {0} place holder is
+            // in the path, it will be replaced with whatever the build
+            // configuration is set to (i.e. DEBUG or RELEASE).
 			var files = Directory.GetFiles(PlugInPath, "*.dll");
 
 			if (files.Length == 0)
@@ -92,20 +119,34 @@ namespace GorgonLibrary.Examples
 			// Find our plug-ins in the DLLs.
 			foreach(var file in files)
 			{
+                // Get the assembly name.  
+                // This is the preferred method of loading a plug-in assembly.
+                // It keeps us from going into DLL hell because it'll contain
+                // version information, public key info, etc...  
 				var name = AssemblyName.GetAssemblyName(file);
 
 				// Skip any assemblies that aren't a plug-in assembly.
+                // Note that this will throw an exception with native (i.e. non-.NET) 
+                // DLLs.  In this case it's OK, because we know there's no native 
+                // DLLs in the default plug-in path.
 				if (!Gorgon.PlugIns.IsPlugInAssembly(name))
 				{
 					continue;
 				}
 
 				// Load the assembly DLL.
+                // This will not only load the assembly DLL into the application
+                // domain (if it's not already loaded), but will also enumerate
+                // the plug-in types.  If there are none, an exception will be
+                // thrown.  This is why we do a check with IsPlugInAssembly before
+                // we load the assembly.
 				Gorgon.PlugIns.LoadPlugInAssembly(name);
 
 				// Now try to retrieve our input plug-ins.
-				var plugIns = Gorgon.PlugIns.EnumeratePlugIns(name);
-				var inputPlugIns = plugIns
+                // Retrieve the list of plug-ins from the assembly.  Once we have
+                // the list we look for any plug-ins that are GorgonInputPlugIn
+                // types and retrieve their type information.
+				var inputPlugIns = Gorgon.PlugIns.EnumeratePlugIns(name)
 									.Where(item => item is GorgonInputPlugIn)
 									.Select(item => item.GetType());
 
@@ -115,9 +156,11 @@ namespace GorgonLibrary.Examples
 					result = new List<Tuple<GorgonInputPlugIn, GorgonInputFactory>>();
 				}
 
-				// Add each input factory to our list.
+				// Add each input factory and plug-in to our list.                
 				foreach (var inputPlugIn in inputPlugIns)
 				{
+                    // Here we actually create the input factory from the plug-in
+                    // by passing the input plug-in type.  
 					var factory = GorgonInputFactory.CreateInputFactory(inputPlugIn);
 
 					if (factory != null)
@@ -141,11 +184,24 @@ namespace GorgonLibrary.Examples
 			try
 			{
 				Console.ForegroundColor = ConsoleColor.White;
+                Console.Title = "Gorgon Example - Input Factories.";
+
+                Console.WriteLine("In this example we will perform the first step in accessing Gorgon's input");
+                Console.WriteLine("device API. Unlike the previous version of Gorgon, this is done by means of");
+                Console.WriteLine("an object called an input factory.  The input factory is responsible for");
+                Console.WriteLine("creating input device objects like a mouse, keyboard or joystick which can");
+                Console.WriteLine("then be used to monitor input from one of these devices.");
+                Console.WriteLine();
+                Console.WriteLine("It is important to note that this example does not create any of these devices");
+                Console.WriteLine("since they require a Windows Form to operate.");
+
+                Console.WriteLine();
 				Console.WriteLine("Loading input plug-ins...");
 
 				// Load our input factory plug-ins.
 				var inputPlugIns = GetInputPlugIns();
 
+                // No plug-ins?  No luck.
 				if (inputPlugIns.Count == 0)
 				{
 					Console.ResetColor();
@@ -154,15 +210,33 @@ namespace GorgonLibrary.Examples
 				}
 				else
 				{
+                    // Display the plug-in information.
 					Console.WriteLine();
-					Console.WriteLine("{0} input factory plug-ins found:", inputPlugIns.Count);
-					Console.ForegroundColor = ConsoleColor.Cyan;
+					Console.WriteLine("{0} input factory plug-ins found:", inputPlugIns.Count);					
 					foreach (var plugIn in inputPlugIns)
 					{
-						Console.WriteLine("{0}", plugIn.Item1.Description);
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+
+                        // Enumerate the devices available on the system.
+                        plugIn.Item2.EnumerateDevices();
+
+                        // The XBox Controller plug-in always registers multiple devices, even when none
+                        // are physically attached to the system.  So in this case, we'll disregard those.
+                        Console.WriteLine("{0}", plugIn.Item1.Description);
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("\t{0} keyboards, {1} mice, {2} joysticks found.", 
+                            plugIn.Item2.KeyboardDevices.Count, 
+                            plugIn.Item2.PointingDevices.Count, 
+                            plugIn.Item2.JoystickDevices.Where(item => item.IsConnected).Count());  
+
+                        // Note that we're checking for connected joysticks, this is because a joystick
+                        // like the XBox Controller is always present, but in a disconnected state.
 					}
+                    Console.ResetColor();
 				}
 
+                Console.WriteLine();
+                Console.WriteLine("Press any key...");
 				Console.ReadKey();
 			}
 			catch (Exception ex)
