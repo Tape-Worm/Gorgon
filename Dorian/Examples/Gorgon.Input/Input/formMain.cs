afc33d17f576f232190053e13be4210b790e6b62
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-01-10 06:55:51 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1132 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Gorgon.Input/Input/formMain.cs
+++ Dorian/Examples/Gorgon.Input/Input/formMain.cs
@@ -34,24 +34,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GorgonLibrary;
-using GorgonLibrary.Input;
+using GorgonLibrary.Diagnostics;
+using GorgonLibrary.Math;
 using GorgonLibrary.UI;
+using GorgonLibrary.Input;
 
 namespace GorgonLibrary.Examples
 {
 	/// <summary>
 	/// Our main form for the example.
 	/// </summary>
+	/// <remarks>
+	/// In this example we will instance and control the mouse (pointing device) and keyboard devices.  So, naturally, you'll
+	/// need a mouse and keyboard for this.  If you don't have one... well... how are you doing anything?
+	/// 
+	/// Unlike the InputFactory example, we're just going to load the Raw Input plug-in and use the keyboard/mouse from that
+	/// interface.  The Raw Input is most useful for mice (although it has a keyboard component) and gives very precise control
+	/// over the mouse.  It allows the developer to lock the mouse to the window while it has focus, or even when the window
+	/// isn't in focus (this of course means no other applications will get access to the mouse).  This isn't the default 
+	/// behaviour, and must be turned on by setting the Exclusive property to true (and AllowBackground to true for background
+	/// exclusive access).  Keyboards also have these properties as well.  Be warned though that when a keyboard goes into
+	/// exclusive mode the standard windows hot keys will no longer be recognized (e.g. Alt+F4, etc..) and must be processed by 
+	/// your application.
+	/// 
+	/// If you're curious about the "BufferContext" stuff, that's all from GDI+ (System.Drawing) and allows us to set up a 
+	/// double buffer scenario so our mouse cursor can be drawn without flicker.  But that is not the scope of this example.
+	/// 
+	/// To create the mouse and keyboard object we need to instance it from the InputFactory that comes from our Raw Input 
+	/// plug-in.  To learn more about loading the InputFactories from their plug-ins see the InputFactory example.
+	/// 
+	/// Once we have these objects via the InputFactory.CreatePointingDevice and InputFactory.CreateKeyboard methods, we can then
+	/// set them up for exclusive access and then assign events that will be triggered by input from the device.  This is a 
+	/// similar setup to the Windows Forms input events (KeyDown, MouseDown, MouseMove, etc...) and is great in an environment 
+	/// where the overhead of events is not an issue.  But in performance critical sections or just for absolute control, these
+	/// objects can be polled at any time (e.g. in your Idle loop method).  This way the application can retrieve data from a 
+	/// device like the mouse in real time, that is, as fast as your computer can call the update loop.  
+	/// 
+	/// Joysticks are exposed in the Raw Input plug-in, but use a very basic interface and don't expose any special features like
+	/// force feedback.  And unlike other devices, joysticks don't raise events and must be polled via its Poll() method.  
+	/// In this example, if a joystick is detected it will be noted on the display panel and pressing the primary button will 
+	/// draw to the display panel.
+	/// </remarks>
 	public partial class formMain : Form
 	{
-		#region Variables.
+		#region Variables.		
 		private BufferedGraphicsContext _graphicsContext = null;		// Buffered graphics context.
 		private BufferedGraphics _buffer = null;						// Buffered graphics page.
 		private Image _mouseImage = null;								// Image to use for double buffering our mouse.
+		private Bitmap _drawingSurface = null;							// Drawing surface for our joystick.
 		private Graphics _imageGraphics = null;							// Graphics interface for the mouse double buffer image.
 		private Graphics _graphics = null;								// GDI+ graphics interface.
 		private GorgonInputFactory _factory = null;						// Our input factory.
 		private GorgonPointingDevice _mouse = null;						// Our mouse interface.
+		private GorgonJoystick _joystick = null;						// A joystick interface.
 		private GorgonKeyboard _keyboard = null;						// Our keyboard interface.
 		private Point _mousePosition = Point.Empty;						// Mouse position.
 		private Image _currentCursor = null;							// Current image for the cursor.
@@ -86,6 +121,12 @@ namespace GorgonLibrary.Examples
 				_imageGraphics = null;
 			}
 
+			if (_drawingSurface != null)
+			{
+				_drawingSurface.Dispose();
+				_drawingSurface = null;
+			}
+
 			if (_mouseImage != null)
 			{
 				_mouseImage.Dispose();
@@ -109,6 +150,7 @@ namespace GorgonLibrary.Examples
 			_graphics = Graphics.FromHwnd(panelDisplay.Handle);
 
 			_mouseImage = new Bitmap(panelDisplay.ClientSize.Width, panelDisplay.ClientSize.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+			_drawingSurface = new Bitmap(panelDisplay.ClientSize.Width, panelDisplay.ClientSize.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
 			_imageGraphics = Graphics.FromImage(_mouseImage);
 
@@ -143,6 +185,32 @@ namespace GorgonLibrary.Examples
         }
 
 		/// <summary>
+		/// Function to draw a spray effect when the joystick button is pressed.
+		/// </summary>
+		/// <param name="stickPoint">The position of the joystick.</param>
+		private void DrawSpray(Point stickPoint)
+		{
+			// Ensure that the joystick is connected and the button is pressed.
+			if ((!_joystick.IsConnected) || (!_joystick.Button[0].IsPressed))
+			{
+				return;
+			}
+
+			// Use random colors.
+			Color color = Color.FromArgb(GorgonRandom.RandomInt32(0, 255), GorgonRandom.RandomInt32(0, 255), GorgonRandom.RandomInt32(0, 255));
+			// Create a random point using the default axis.
+			Point randomPoint = new Point(
+										GorgonRandom.RandomInt32(-10, 10) + stickPoint.X
+										, GorgonRandom.RandomInt32(-10, 10) + stickPoint.Y);
+
+			// Ensure that the random point falls within the range of the image.
+			if (panelDisplay.ClientRectangle.Contains(randomPoint))
+			{
+				_drawingSurface.SetPixel(randomPoint.X, randomPoint.Y, color);
+			}
+		}
+
+		/// <summary>
 		/// Function to process during application idle time.
 		/// </summary>
 		/// <returns>TRUE to continue processing, FALSE to stop.</returns>
@@ -157,6 +225,43 @@ namespace GorgonLibrary.Examples
 
 			// Render our mouse cursor.
 			_buffer.Graphics.Clear(panelDisplay.BackColor);
+			
+			if (_joystick != null) 
+			{
+				// We -must- call poll here or else the joystick will appear to be
+				// disconnected and will not have any current data.
+				_joystick.Poll();
+
+				// Get our joystick data and constrain it.
+				// First get the normalized joystick value.
+				// The stick values are often between a negative value and a positive value.
+				PointF stickNormalized = new PointF(((float)_joystick.X / (float)_joystick.Capabilities.XAxisRange.Range) + 0.5f
+													, ((float)_joystick.Y / (float)_joystick.Capabilities.YAxisRange.Range) + 0.5f);
+
+				// Now transform the normalized point into display space.
+				Point stickPoint = new Point((int)(stickNormalized.X * panelDisplay.ClientSize.Width)
+											,panelDisplay.Height - (int)(stickNormalized.Y * panelDisplay.ClientSize.Height));
+
+				SizeF textSize = SizeF.Empty;
+				string joystickText = string.Format("{0} {1}\nPosition: {2}x{3} (Raw {5}x{6})\nPrimary button {4}"
+											, _joystick.Name
+											, (_joystick.IsConnected ? "connected." : "not connected.")
+											, stickPoint.X
+											, stickPoint.Y
+											, (_joystick.Button[0].IsPressed ? "pressed" : "not pressed (press it for pixel spray).")
+											, _joystick.X
+											, _joystick.Y);
+
+				
+				DrawSpray(stickPoint);
+				_buffer.Graphics.DrawImage(_drawingSurface, Point.Empty);
+
+				// Display joystick info.
+				textSize = _buffer.Graphics.MeasureString(joystickText, this.Font, panelDisplay.ClientSize.Width);
+				_buffer.Graphics.FillRectangle(Brushes.White, Rectangle.Round(new RectangleF(Point.Empty, textSize)));
+				_buffer.Graphics.DrawString(joystickText, this.Font, Brushes.Black, PointF.Empty);
+			}
+
 			_buffer.Graphics.DrawImage(_currentCursor, _mousePosition);
 			_buffer.Render();
 			_buffer.Render(_graphics);
@@ -289,13 +394,13 @@ namespace GorgonLibrary.Examples
 		}
 
 		/// <summary>
-		/// Function called when the window is resized.
+		/// Handles the Resize event of the panelDisplay control.
 		/// </summary>
-		/// <param name="e">An <see cref="T:System.EventArgs" /> that contains the event data.</param>
-		protected override void OnResize(EventArgs e)
+		/// <param name="sender">The source of the event.</param>
+		/// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
+		/// <exception cref="System.NotImplementedException"></exception>
+		private void panelDisplay_Resize(object sender, EventArgs e)
 		{
-			base.OnResize(e);
-
 			if (WindowState == FormWindowState.Minimized)
 			{
 				return;
@@ -358,6 +463,22 @@ namespace GorgonLibrary.Examples
 				_mouse = _factory.CreatePointingDevice(this);
 				_keyboard = _factory.CreateKeyboard(this);
 
+				// If we have a joystick controller, then let's activate it.
+				if (_factory.JoystickDevices.Count > 0)
+				{
+					// Find the first one that's active.
+					var activeDevice = (from joystick in _factory.JoystickDevices
+										where joystick.IsConnected
+										select joystick).FirstOrDefault();
+
+					if (activeDevice != null)
+					{
+						// Note that joysticks from Raw Input are always exclusive access,
+						// so setting _joystick.Exclusive = true; does nothing.
+						_joystick = _factory.CreateJoystick(this, activeDevice.Name);
+					}
+				}
+
 				// Enable the devices.
 				_mouse.Enabled = true;
 				_keyboard.Enabled = true;
@@ -373,12 +494,16 @@ namespace GorgonLibrary.Examples
 				_mouse.PointingDeviceDown += _mouse_PointingDeviceDown;
 				_mouse.PointingDeviceUp += _mouse_PointingDeviceUp;
 
-				// Limit the mouse position to the client area of the window.
-				_mouse.PositionRange = new RectangleF(0, 0, panelDisplay.ClientSize.Width, panelDisplay.ClientSize.Height);
-
 				// Set up an event handler for our keyboard.
 				_keyboard.KeyDown += _keyboard_KeyDown;
 				_keyboard.KeyUp += _keyboard_KeyUp;
+
+				// Limit the mouse position to the client area of the window.				
+				_mouse.PositionRange = new RectangleF(0, 0, panelDisplay.ClientSize.Width, panelDisplay.ClientSize.Height);
+
+				// When the display area changes size, update the buffers
+				// and the limits for the mouse.
+				panelDisplay.Resize += panelDisplay_Resize;
 
 				UpdateKeyboardLabel(KeyboardKeys.None, KeyboardKeys.None);
                 UpdateMouse(_mouse.Position, _mouse.Button);
