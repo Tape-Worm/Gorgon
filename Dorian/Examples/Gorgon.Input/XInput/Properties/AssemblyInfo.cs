06a291beb936cbcbebc0ccd7de66c5fb0851adc9
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-01-11 15:30:15 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1149 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Examples/Gorgon.Input/XInput/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#if DEBUG
+[assembly: AssemblyTitle("Gorgon Input Example [DEBUG VERSION]")]
+[assembly: AssemblyDescription("An example showing how to use the XBox controller. [DEBUG VERSION]")]
+[assembly: AssemblyConfiguration("DEBUG")]
+[assembly: AssemblyProduct("Gorgon [DEBUG VERSON]")]
+#else
+[assembly: AssemblyTitle("Gorgon Input Example")]
+[assembly: AssemblyDescription("An example showing how to use the XBox controller.")]
+[assembly: AssemblyConfiguration("RELEASE")]
+[assembly: AssemblyProduct("Gorgon")]
+#endif
+[assembly: AssemblyCompany("Michael Winsor")]
+[assembly: AssemblyCopyright("Copyright © Michael Winsor 2013")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("edcb9f56-2610-424e-99eb-bcf21dff58c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
