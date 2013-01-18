7138ca80b08e23cfe72580703d95ec48fb66c8a8
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-01-18 15:50:42 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1162 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Examples/Gorgon.FileSystem/Writing/formMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GorgonLibrary.Examples
+{
+	partial class formMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.imageTree = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageTree
+            // 
+            this.imageTree.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageTree.ImageStream")));
+            this.imageTree.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageTree.Images.SetKeyName(0, "folder_16x16.png");
+            this.imageTree.Images.SetKeyName(1, "document_text_16x16.png");
+            this.imageTree.Images.SetKeyName(2, "packedfile_16x16.png");
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gorgon Example - Writing to a Virtual File System";
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+        private System.Windows.Forms.ImageList imageTree;
+	}
+}
+
---
+++ Dorian/Examples/Gorgon.FileSystem/Writing/formMain.cs
@@ -0,0 +1,80 @@
+﻿#region MIT.
+// 
+// Gorgon.
+// Copyright (C) 2013 Michael Winsor
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// Created: Friday, January 18, 2013 8:47:30 AM
+// 
+#endregion
+
+using System;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using GorgonLibrary.FileSystem;
+using GorgonLibrary.UI;
+
+namespace GorgonLibrary.Examples
+{
+	/// <summary>
+	/// Main application interface.
+	/// </summary>
+	/// <remarks>
+	/// </remarks>
+	public partial class formMain : Form
+	{
+		#region Variables.
+		private GorgonFileSystem _fileSystem = null;			// Our file system.
+		#endregion
+
+		#region Methods.
+		/// <summary>
+		/// Raises the <see cref="E:System.Windows.Forms.Form.Load" /> event.
+		/// </summary>
+		/// <param name="e">An <see cref="T:System.EventArgs" /> that contains the event data.</param>
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			try
+			{
+			}
+			catch (Exception ex)
+			{
+				GorgonException.Catch(ex, () => GorgonDialogs.ErrorBox(this, ex));
+				Application.Exit();
+			}
+		}
+		#endregion
+
+		#region Constructor/Destructor.
+		/// <summary>
+		/// Initializes a new instance of the <see cref="formMain" /> class.
+		/// </summary>
+		public formMain()
+		{
+			InitializeComponent();
+		}
+		#endregion
+	}
+}
