8349a86b05916a7503589b5f1004da42cf304843
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-01-21 15:52:06 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1168 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Gorgon.FileSystem/BZipFileSystemOPower/Properties/AssemblyInfo.cs
+++ Dorian/Examples/Gorgon.FileSystem/BZipFileSystemOPower/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿#region MIT.
 // 
 // Gorgon.
-// Copyright (C) 2008 Michael Winsor
+// Copyright (C) 2013 Michael Winsor
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,37 +20,41 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 // 
-// Created: TOBEREPLACED
+// Created: Monday, January 21, 2013 8:45:05 AM
 // 
 #endregion
 
-using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
-using System.Security.Permissions;
 
-//
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//
-[assembly: AssemblyTitle("BZip Filesystem O' Power.")]
-[assembly: AssemblyDescription("Example showing how to use the filesystem functionality of Gorgon.")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+[assembly: AssemblyTitle("BZip FileSystem of Power. [DEBUG VERSION]")]
+[assembly: AssemblyDescription("Example showing how to use the virtual file system.  This was a version 1.x example, converted to version 2.x of Gorgon. [DEBUG VERSION]")]
+[assembly: AssemblyConfiguration("DEBUG")]
+[assembly: AssemblyProduct("Version 1.x Gorgon Example. [DEBUG VERSION]")]
+#else
+[assembly: AssemblyTitle("BZip FileSystem of Power.")]
+[assembly: AssemblyDescription("Example showing how to use the virtual file system.  This was a version 1.x example, converted to version 2.x of Gorgon.")]
+[assembly: AssemblyConfiguration("RELEASE")]
+[assembly: AssemblyProduct("Version 1.x Gorgon Example.")]
+#endif
 [assembly: AssemblyCompany("Michael Winsor")]
-[assembly: AssemblyProduct("Gorgon Example.")]
 [assembly: AssemblyCopyright("Copyright © Michael Winsor")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(false)]
-[assembly: FileIOPermission(SecurityAction.RequestMinimum, AllFiles = FileIOPermissionAccess.AllAccess)]
-[assembly: ReflectionPermission(SecurityAction.RequestMinimum, Flags = ReflectionPermissionFlag.MemberAccess)]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode = true)]
 
-//
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0099a968-8152-43d9-98f2-213a21efc80e")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -58,34 +62,5 @@ using System.Security.Permissions;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.1.*")]
-
-//
-// In order to sign your assembly you must specify a key to use. Refer to the 
-// Microsoft .NET Framework documentation for more information on assembly signing.
-//
-// Use the attributes below to control which key is used for signing. 
-//
-// Notes: 
-//   (*) If no key is specified, the assembly is not signed.
-//   (*) KeyName refers to a key that has been installed in the Crypto Service
-//       Provider (CSP) on your machine. KeyFile refers to a file which contains
-//       a key.
-//   (*) If the KeyFile and the KeyName values are both specified, the 
-//       following processing occurs:
-//       (1) If the KeyName can be found in the CSP, that key is used.
-//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
-//           in the KeyFile is installed into the CSP and used.
-//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
-//       When specifying the KeyFile, the location of the KeyFile should be
-//       relative to the project output directory which is
-//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
-//       located in the project directory, you would specify the AssemblyKeyFile 
-//       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
-//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
-//       documentation for more information on this.
-//
-[assembly: AssemblyFileVersion("1.1.4119.34319")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
