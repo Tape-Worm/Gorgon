159e48859bf73ea5d686d02cbc5b2f7b88c56d35
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-01-29 06:57:52 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1203 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Examples/Tester_Graphics/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SlimMath;
+using GorgonLibrary;
+using GorgonLibrary.UI;
+using GorgonLibrary.Graphics;
+
+namespace Tester_Graphics
+{
+	public partial class Form2 : Form
+	{
+		private GorgonGraphics _graphics = null;
+		private GorgonSwapChain _swap = null;
+
+		private bool Idle()
+		{
+			_swap.Clear(Color.White);
+
+			_swap.Flip();
+			return true;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+
+			try
+			{
+				_graphics = new GorgonGraphics();
+				_swap = _graphics.Output.CreateSwapChain("Screen", new GorgonSwapChainSettings()
+				{
+					IsWindowed = true,
+					Width = 800,
+					Height = 600,
+					Format = BufferFormat.R8G8B8A8_UIntNormal
+				});
+
+				
+
+				Gorgon.ApplicationIdleLoopMethod = Idle;
+			}
+			catch (Exception ex)
+			{
+				GorgonDialogs.ErrorBox(this, ex);
+			}
+		}
+
+		public Form2()
+		{
+			InitializeComponent();
+		}
+	}
+}
