edf8dd01aa8d11b9a32f5f8b312b42adf93eba89
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-02-15 17:04:55 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1232 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Tester_Graphics/Form2.cs
+++ Dorian/Examples/Tester_Graphics/Form2.cs
@@ -22,51 +22,63 @@ namespace Tester_Graphics
 	// KEEP THIS CODE!!!  Use in example for loading animated GIF files!
 	public partial class Form2 : Form
 	{
+        private GorgonRenderTarget _target = null;
 		private GorgonConstantBuffer _frameIndex = null;
 		private GorgonDataStream _stream = null;
 		private GorgonGraphics _graphics = null;
 		private GorgonSwapChain _swap = null;
 		private Gorgon2D _2D = null;
 		private GorgonSprite _sprite = null;
+        private GorgonSprite _targetSprite = null;
 		private GorgonTexture2D _texture = null;
 		private GorgonTimer _timer = null;
 		private GorgonPixelShader _shader = null;
-		private int _frame = 1;
+        private int[] _delays = null;
+		private int _frame = 0;
 
 		private bool Idle()
 		{
-			_2D.Clear(Color.Black);
-
-			_sprite.Opacity += 0.15f * GorgonTiming.Delta;
-			_sprite.Opacity = 1.0f.Min(_sprite.Opacity);
+            _2D.PixelShader.Current = _shader;
+            _2D.PixelShader.ConstantBuffers[2] = _frameIndex;
+            _2D.Target = _target;            
 
 			_sprite.Draw();
+
+            _2D.PixelShader.Current = null;
+            _2D.Target = null;
+            _2D.Clear(Color.White);
+
+            _targetSprite.Size = new Vector2(_swap.Settings.Width, _swap.Settings.Height);
+            _targetSprite.Opacity += 0.25f * GorgonTiming.Delta;
+            _targetSprite.Opacity = 1.0f.Min(_targetSprite.Opacity);
+            _targetSprite.Draw();
 
 			if (_timer == null)
 			{
 				_timer = new GorgonTimer();
 			}
 
-			if (_timer.Milliseconds >= 70)
+            int delay = _delays[_frame] * 10;
+			if (_timer.Milliseconds > delay)
 			{
 				_timer.Reset();
 				_frame++;
 
 				if (_frame >= _texture.Settings.ArrayCount)
 				{
-					_frame = 1;
+					_frame = 0;
 				}
 
-				UpdateFrame();
+				UpdateFrame(_frame);
 			}
 
 			_2D.Render();
 			return true;
 		}
 
-		private void UpdateFrame()
+		private void UpdateFrame(int frame)
 		{
-			_stream.Write<float>(_frame);
+			_stream.Write<float>(frame);
 			_stream.Position = 0;
 			_frameIndex.Update(_stream);
 		}
@@ -98,9 +110,11 @@ namespace Tester_Graphics
 				});
 
 				//GorgonImageCodecs.DDS.LegacyConversionFlags = DDSFlags.NoR10B10G10A2Fix;
+                string fileName = @"c:\mike\unpak\moondance.gif";
 				GorgonImageCodecs.GIF.UseAllFrames = true;
 				GorgonImageCodecs.GIF.Clip = true;	// Clip this image because animated gifs can have varying frame sizes and resizing the frames can cause issues.
-				using (var data = GorgonImageData.FromFile(@"d:\images\rain_test.gif"))
+                _delays = GorgonImageCodecs.GIF.GetFrameDelays(fileName);
+				using (var data = GorgonImageData.FromFile(fileName))
 				{
                     _texture = _graphics.Textures.CreateTexture<GorgonTexture2D>("Test", (GorgonTexture2DSettings)data.Settings, data);
 				}
@@ -108,16 +122,25 @@ namespace Tester_Graphics
 				_2D = _graphics.Output.Create2DRenderer(_swap);
 				
 				_sprite = _2D.Renderables.CreateSprite("Test", new Vector2(_texture.Settings.Width, _texture.Settings.Height), _texture);
-				_sprite.Size = new Vector2(_swap.Settings.Width, _swap.Settings.Height);
-				_sprite.Opacity = 0.0f;
+                _sprite.Blending.SourceAlphaBlend = BlendType.One;
+                _sprite.Blending.DestinationAlphaBlend = BlendType.One;
 
 				_frameIndex = _graphics.Shaders.CreateConstantBuffer(sizeof(float) * 4, false);
 				_stream = new GorgonDataStream(sizeof(float));
+                UpdateFrame(0);
 
 				_shader = _graphics.Shaders.CreateShader<GorgonPixelShader>("TArray", "DualTex", Properties.Resources.Shader, true);
 
-				_2D.PixelShader.Current = _shader;
-				_2D.PixelShader.ConstantBuffers[0] = _frameIndex;
+                _target = _graphics.Output.CreateRenderTarget("Name", new GorgonRenderTargetSettings()
+                {
+                    Width = (int)_sprite.Size.X,
+                    Height = (int)_sprite.Size.Y,
+                    Format = BufferFormat.R8G8B8A8_UIntNormal
+                });
+
+                _target.Clear(Color.Transparent);
+                _targetSprite = _2D.Renderables.CreateSprite("Target", new Vector2(_target.Settings.Width, _target.Settings.Height), _target.Texture);
+                _targetSprite.Opacity = 0.0f;
 
                 /*using (var texture = _graphics.Textures.FromFile<GorgonTexture2D>("Test", @"d:\unpak\textureUpload.dds", new GorgonTexture2DSettings()
                 {
