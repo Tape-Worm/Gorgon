25e38671a4079dfab0bdd99d4caf0ce849ede348
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2013-08-08 20:53:55 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1505 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Test/Gorgon.Common.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GorgonLibrary;
+
+namespace Gorgon.Common.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGenerateHash()
+        {
+            var values = new List<int>();
+
+            for (int i = 0; i < 32767; i++)
+            {
+                int hash = 281;
+
+                for (int mem = 0; mem < i+1; mem++)
+                {
+                    hash = hash.GenerateHash(mem);
+                }
+
+                Assert.IsFalse(values.Contains(hash), "Hash " + hash + " was duplicated.");
+
+                values.Add(hash);
+            }
+        }
+    }
+}
