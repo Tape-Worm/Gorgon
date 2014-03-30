d91eb1695a17a15c2726b569eaa86ad1dbdcc2ec
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2014-03-30 08:21:01 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1689 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Gorgon/Gorgon.Renderers/2D/Renderables/GorgonPoint.cs
+++ Dorian/Gorgon/Gorgon.Renderers/2D/Renderables/GorgonPoint.cs
@@ -472,6 +472,17 @@ namespace GorgonLibrary.Renderers
 		}
 
 		/// <summary>
+		/// Function to force an update to the renderable object.
+		/// </summary>
+		/// <remarks>
+		/// Take care when calling this method repeatedly.  It will have a significant performance impact.
+		/// </remarks>
+		public void Refresh()
+		{
+			// Do nothing since we don't care about redundant calls.
+		}
+
+		/// <summary>
 		/// Function to draw the object.
 		/// </summary>
 		/// <remarks>Please note that this doesn't draw the object to the target right away, but queues it up to be 
