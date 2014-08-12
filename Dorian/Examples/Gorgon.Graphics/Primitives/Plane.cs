ebf6a27e48f777e3533be1f6341431fd4e76c10c
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2014-08-12 06:08:59 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1787 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

--- Dorian/Examples/Gorgon.Graphics/Primitives/Plane.cs
+++ Dorian/Examples/Gorgon.Graphics/Primitives/Plane.cs
@@ -37,124 +37,13 @@ using SlimMath;
 namespace GorgonLibrary.Graphics.Example
 {
 	class Plane
-		: IPrimitive, IDisposable
+		: MoveableMesh, IPrimitive, IDisposable
 	{
 		#region Variables.
 		// Flag to indicate that the object was disposed.
 		private bool _disposed;
-		// Position of the plane.
-		private Vector3 _position = Vector3.Zero;
-		// Angle of the plane.
-		private Vector3 _rotation = Vector3.Zero;
-		// Scale of the plane.
-		private Vector3 _scale = new Vector3(1);
-		// Cached position matrix.
-		private Matrix _posMatrix = Matrix.Identity;
-		// Cached rotation matrix.
-		private Matrix _rotMatrix = Matrix.Identity;
-		// Cached scale matrix.
-		private Matrix _scaleMatrix = Matrix.Identity;
-		// Cached world matrix.
-		private Matrix _world = Matrix.Identity;
 		// Initial orientation.
 		private Matrix _orientation = Matrix.Identity;
-		// Flags to indicate that the object needs to update its transform.
-		private bool _needsPosTransform;
-		private bool _needsSclTransform;
-		private bool _needsRotTransform;
-		private bool _needsWorldUpdate;
-		#endregion
-
-		#region Properties.
-		/// <summary>
-		/// Property to set or return the current position of the triangle.
-		/// </summary>
-		public Vector3 Position
-		{
-			get
-			{
-				return _position;
-			}
-			set
-			{
-				_position = value;
-				_needsPosTransform = true;
-				_needsWorldUpdate = true;
-			}
-		}
-
-		/// <summary>
-		/// Property to set or return the angle of rotation for the triangle (in degrees).
-		/// </summary>
-		public Vector3 Rotation
-		{
-			get
-			{
-				return _rotation;
-			}
-			set
-			{
-				_rotation = value;
-				_needsRotTransform = true;
-				_needsWorldUpdate = true;
-			}
-		}
-
-		/// <summary>
-		/// Property to set or return the scale of the transform.
-		/// </summary>
-		public Vector3 Scale
-		{
-			get
-			{
-				return _scale;
-			}
-			set
-			{
-				_scale = value;
-				_needsSclTransform = true;
-				_needsWorldUpdate = true;
-			}
-		}
-
-		/// <summary>
-		/// Property to return the world matrix for this triangle.
-		/// </summary>
-		public Matrix World
-		{
-			get
-			{
-				if (!_needsWorldUpdate)
-				{
-					return _world;
-				}
-
-				if (_needsPosTransform)
-				{
-					Matrix.Translation(ref _position, out _posMatrix);
-					_needsPosTransform = false;
-				}
-
-				if (_needsRotTransform)
-				{
-					var rads = new Vector3(_rotation.X.Radians(), _rotation.Y.Radians(), _rotation.Z.Radians());
-					Matrix.RotationYawPitchRoll(rads.Y, rads.X, rads.Z, out _rotMatrix);
-					_needsRotTransform = false;
-				}
-
-				if (_needsSclTransform)
-				{
-					Matrix.Scaling(ref _scale, out _scaleMatrix);
-					_needsSclTransform = false;
-				}
-
-
-				Matrix.Multiply(ref _rotMatrix, ref _scaleMatrix, out _world);
-				Matrix.Multiply(ref _world, ref _posMatrix, out _world);
-
-				return _world;
-			}
-		}
 		#endregion
 
 		#region Methods.
