5d984440116a9fa210af2abacc78b6a437b5c6f2
tape.wyrm <tape.wyrm@a61f9e97-bd4c-0410-8c65-85b29eaa70ff>
2014-10-02 15:23:04 +0000

    git-svn-id: https://gorgonlib.googlecode.com/svn/branches/2.x@1826 a61f9e97-bd4c-0410-8c65-85b29eaa70ff

---
+++ Dorian/Tools/Gorgon.Editor.PlugIns/Gorgon.ImageEditor/Resources/ImageViewShaders.hlsl
@@ -0,0 +1,44 @@
+﻿// Shaders for image viewing.
+#GorgonInclude "Gorgon2DShaders"
+
+// Our default texture and sampler.
+Texture1D _gorgonTexture1D : register(t0);
+Texture3D _gorgonTexture3D : register(t0);
+
+// The depth/array index to view.
+cbuffer GorgonDepthArrayIndex {
+	float depthArrayIndex : register(b1)
+}
+
+// Pixel shader to view a 1D texture.
+float4 Gorgon1DTextureView(GorgonSpriteVertex vertex) : SV_Target
+{
+	float2 coords = float2(vertex.u, depthArrayIndex);
+	float4 color = _gorgonTexture1D.Sample(_gorgonSampler, coords);
+
+	REJECT_ALPHA(color.a);
+		
+	return color;
+}
+
+// Pixel shader to view a 1D texture.
+float4 Gorgon2DTextureView(GorgonSpriteVertex vertex) : SV_Target
+{
+	float3 coords = float3(vertex.u, vertex.v, depthArrayIndex);
+	float4 color = _gorgonTexture1D.Sample(_gorgonSampler, coords);
+
+	REJECT_ALPHA(color.a);
+		
+	return color;
+}
+
+// Pixel shader to view a 3D texture.
+float4 Gorgon3DTextureView(GorgonSpriteVertex vertex) : SV_Target
+{
+	float3 coords = float3(vertex.u, vertex.v, depthArrayIndex);
+	float4 color = _gorgonTexture3D.Sample(_gorgonSampler, coords);
+
+	REJECT_ALPHA(color.a);
+		
+	return color;
+}
