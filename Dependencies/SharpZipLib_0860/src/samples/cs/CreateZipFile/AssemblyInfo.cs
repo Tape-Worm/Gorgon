5cb2a21ea79d7c5d2568e0aec7a1981b3e9cd4bd
Michael Winsor <github@tape-worm.net>
2014-11-13 22:58:13 -0700

    Changed naming
    Updated naming scheme for branch.

---
+++ Dependencies/SharpZipLib_0860/src/samples/cs/CreateZipFile/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CreateZipFile")]
+[assembly: AssemblyDescription("Free C# IDE")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("A SharpDevelop sample")]
+[assembly: AssemblyCopyright("Mike Krueger 2000")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.85.4.369")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
