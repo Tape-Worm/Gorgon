2da7d1327c9560f64346d63842ee49e13395fe0a
Michael Winsor <github@tape-worm.net>
2015-07-21 00:20:15 -0600

    Missed input test
    Build is broken. Will not compile.

---
+++ Test/Gorgon.Input.Test/Mock/MockControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Gorgon.Input.Test.Mock
+{
+	public partial class MockControl : Form
+	{
+		public MockControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Test/Gorgon.Input.Test/Mock/MockInputServiceFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Gorgon.Input.Test.Mock
+{
+	class MockInputServiceFactory
+		: IGorgonInputServiceFactory
+	{
+
+		#region IGorgonInputServiceFactory Members
+
+		public IGorgonInputService CreateService(string servicePluginName)
+		{
+			return new MockInputService();
+		}
+
+		public IEnumerable<IGorgonInputService> CreateServices(AssemblyName pluginAssembly = null)
+		{
+			return new[]
+			       {
+				       new MockInputService(),
+				       new MockInputService()
+			       };
+		}
+
+		#endregion
+	}
+}
