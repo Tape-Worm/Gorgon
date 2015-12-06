1e6ac8aea2c64c829f466fca29a3b5b29bc6734b
Michael Winsor <github@tape-worm.net>
2015-12-05 22:32:14 -0700

    Removing inheritdoc tags

--- PlugIns/Gorgon.FileSystem.Zip/ZipProvider.cs
+++ PlugIns/Gorgon.FileSystem.Zip/ZipProvider.cs
@@ -48,13 +48,35 @@ namespace Gorgon.IO.Zip
 		#endregion
 
 		#region Methods.
-		/// <inheritdoc/>
-		protected override GorgonPhysicalFileSystemData OnEnumerate(string physicalMountPoint, IGorgonVirtualDirectory mountPoint)
+		/// <summary>
+		/// Function to enumerate the files and directories from a physical location and map it to a virtual location.
+		/// </summary>
+		/// <param name="physicalLocation">The physical location containing files and directories to enumerate.</param>
+		/// <param name="mountPoint">A <see cref="IGorgonVirtualDirectory"/> that the directories and files from the physical file system will be mounted into.</param>		
+		/// <returns>A <see cref="GorgonPhysicalFileSystemData"/> object containing information about the directories and files contained within the physical file system.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when the <paramref name="physicalLocation"/>, or the <paramref name="mountPoint"/> parameters are <b>null</b> (<i>Nothing</i> in VB.Net).</exception>
+		/// <exception cref="ArgumentException">Thrown when the <paramref name="physicalLocation"/> parameter is empty.</exception>
+		/// <remarks>
+		/// <para>
+		/// This will return a <see cref="GorgonPhysicalFileSystemData"/> representing the paths to directories and <see cref="IGorgonPhysicalFileInfo"/> objects under the virtual file system. Each file 
+		/// system file and directory is mapped from its <paramref name="physicalLocation"/> on the physical file system to a <paramref name="mountPoint"/> on the virtual file system. For example, if the 
+		/// mount point is set to <c>/MyMount/</c>, and the physical location of a file is <c>c:\SourceFileSystem\MyDirectory\MyTextFile.txt</c>, then the returned value should be 
+		/// <c>/MyMount/MyDirectory/MyTextFile.txt</c>.
+		/// </para>
+		/// <para>
+		/// Implementors of a <see cref="GorgonFileSystemProvider"/> plug in can override this method to read the list of files from another type of file system, like a Zip file.
+		/// </para>
+		/// <para>
+		/// Implementors of a <see cref="GorgonFileSystemProvider"/> should override this method to read the list of directories and files from another type of file system, like a Zip file. 
+		/// The default functionality will only enumerate directories and files from the operating system file system.
+		/// </para>
+		/// </remarks>
+		protected override GorgonPhysicalFileSystemData OnEnumerate(string physicalLocation, IGorgonVirtualDirectory mountPoint)
 		{
             var directories = new List<string>();
             var files = new List<IGorgonPhysicalFileInfo>();
 
-			using (var zipStream = new ZipInputStream(File.Open(physicalMountPoint, FileMode.Open, FileAccess.Read, FileShare.Read)))
+			using (var zipStream = new ZipInputStream(File.Open(physicalLocation, FileMode.Open, FileAccess.Read, FileShare.Read)))
 			{
 				ZipEntry entry;
 
@@ -77,7 +99,7 @@ namespace Gorgon.IO.Zip
 							directories.Add(directoryName);
 						}
 
-						files.Add(new ZipPhysicalFileInfo(entry, physicalMountPoint, mountPoint));
+						files.Add(new ZipPhysicalFileInfo(entry, physicalLocation, mountPoint));
 					}
 					else
 					{
@@ -89,13 +111,50 @@ namespace Gorgon.IO.Zip
 			return new GorgonPhysicalFileSystemData(directories, files);
 		}
 
-		/// <inheritdoc/>
+		/// <summary>
+		/// Function to open a stream to a file on the physical file system from the <see cref="IGorgonVirtualFile"/> passed in.
+		/// </summary>
+		/// <param name="file">The <see cref="IGorgonVirtualFile"/> that will be used to locate the file that will be opened on the physical file system.</param>
+		/// <returns>A <see cref="Stream"/> to the file, or <b>null</b> (<i>Nothing</i> in VB.Net) if the file does not exist.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when the <paramref name="file"/> parameter is <b>null</b> (<i>Nothing</i> in VB.Net).</exception>
+		/// <remarks>
+		/// <para>
+		/// This will take the <see cref="IGorgonVirtualFile"/> and open its corresponding physical file location as a stream for reading. The stream that is returned will be opened, and as such, it is the 
+		/// responsibility of the user to close the stream when finished.
+		/// </para>
+		/// <para>
+		/// If the file does not exist in the physical file system, this method should return <b>null</b> (<i>Nothing</i> in VB.Net).
+		/// </para>
+		/// <para>
+		/// Implementors of a <see cref="GorgonFileSystemProvider"/> plug in can overload this method to return a stream into a file within their specific native provider (e.g. a Zip file provider will 
+		/// return a stream into the zip file positioned at the location of the compressed file within the zip file).
+		/// </para>
+		/// </remarks>
 		protected override GorgonFileSystemStream OnOpenFileStream(IGorgonVirtualFile file)
 		{
 			return new ZipFileStream(file, File.Open(file.MountPoint.PhysicalPath, FileMode.Open, FileAccess.Read, FileShare.Read));
-		} 
+		}
 
-		/// <inheritdoc/>
+		/// <summary>
+		/// Function to determine if a physical file system can be read by this provider.
+		/// </summary>
+		/// <param name="physicalPath">Path to the packed file containing the file system.</param>
+		/// <returns><b>true</b> if the provider can read the packed file, <b>false</b> if not.</returns>
+		/// <exception cref="System.ArgumentNullException">Thrown when the <paramref name="physicalPath"/> parameter is NULL (<i>Nothing</i> in VB.Net).</exception>
+		/// <exception cref="System.ArgumentException">Thrown when the <paramref name="physicalPath"/> parameter is an empty string.</exception>
+		/// <remarks>
+		/// <para>
+		/// This will test a physical file system (e.g. a Zip file) to see if the provider can open it or not. If used with a directory on an operating system file system, this method should always return 
+		/// <b>false</b>.
+		/// </para>
+		/// <para>
+		/// When used with a <see cref="IGorgonFileSystemProvider"/> that supports a non operating system based physical file system, such as the <see cref="GorgonFileSystemRamDiskProvider"/>, then this 
+		/// method should compare the <paramref name="physicalPath"/> with its <see cref="IGorgonFileSystemProvider.Prefix"/> to ensure that the <see cref="IGorgonFileSystem"/> requesting the provider is using the correct provider.
+		/// </para>
+		/// <para>
+		/// Implementors of a <see cref="GorgonFileSystemProvider"/> should override this method to determine if a packed file can be read by reading the header of the file specified in <paramref name="physicalPath"/>.
+		/// </para>
+		/// </remarks>
 		protected override bool OnCanReadFile(string physicalPath)
 		{
 		    var headerBytes = new byte[4];
