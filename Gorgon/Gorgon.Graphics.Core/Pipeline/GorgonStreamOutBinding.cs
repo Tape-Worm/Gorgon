1f98e4b11357c9114da5322e0031ec6e4b22a0e5
Michael Winsor <github@tape-worm.net>
2017-08-01 21:23:31 -0600

    Fixed bug where stream out vertex/index buffers were not being detected by enum, causing compilation error.

--- Gorgon/Gorgon.Graphics.Core/Pipeline/GorgonStreamOutBinding.cs
+++ Gorgon/Gorgon.Graphics.Core/Pipeline/GorgonStreamOutBinding.cs
@@ -86,12 +86,12 @@ namespace Gorgon.Graphics.Core
                 throw new GorgonException(GorgonResult.CannotCreate, Resources.GORGFX_ERR_BUFFER_CONSTANT_NO_SO);
             }
 
-            if ((buffer is GorgonIndexBuffer indexBuffer) && (!indexBuffer.Info.IsStreamOut))
+            if ((buffer is GorgonIndexBuffer indexBuffer) && ((indexBuffer.Info.Binding & VertexIndexBufferBinding.StreamOut) != VertexIndexBufferBinding.StreamOut))
             {
                 throw new GorgonException(GorgonResult.CannotCreate, string.Format(Resources.GORGFX_ERR_BUFFER_TYPE_MISSING_SO, buffer.Name));
             }
 
-            if ((buffer is GorgonVertexBuffer vertexBuffer) && (!vertexBuffer.Info.IsStreamOut))
+            if ((buffer is GorgonVertexBuffer vertexBuffer) && ((vertexBuffer.Info.Binding & VertexIndexBufferBinding.StreamOut) != VertexIndexBufferBinding.StreamOut))
             {
                 throw new GorgonException(GorgonResult.CannotCreate, string.Format(Resources.GORGFX_ERR_BUFFER_TYPE_MISSING_SO, buffer.Name));
             }
