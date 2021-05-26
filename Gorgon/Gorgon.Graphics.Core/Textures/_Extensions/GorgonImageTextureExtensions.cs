11f219cf3c6eaf9ae5c45a624d95b26a836550d5
Michael Winsor <github@tape-worm.net>
2021-05-25 19:36:37 -0600

    [Graphics] - Added utility functions to create a 1D/2D/3D texture view from a IGorgonImage, or GDI+ bitmap (2D Only).

--- Gorgon/Gorgon.Graphics.Core/Textures/_Extensions/GorgonImageTextureExtensions.cs
+++ Gorgon/Gorgon.Graphics.Core/Textures/_Extensions/GorgonImageTextureExtensions.cs
@@ -115,6 +115,167 @@ namespace Gorgon.Graphics.Core
         }
 
         /// <summary>
+        /// Function to create a <see cref="GorgonTexture2DView"/> from a GDI+ bitmap.
+        /// </summary>
+        /// <param name="gdiBitmap">The GDI+ bitmap used to create the texture.</param>
+        /// <param name="graphics">The graphics interface used to create the texture.</param>
+        /// <param name="options">[Optional] Options used to further define the texture.</param>
+        /// <returns>A new <see cref="GorgonTexture2D"/> containing the data from the <paramref name="gdiBitmap"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="gdiBitmap"/>, or the <paramref name="graphics"/> parameter is <b>null</b>.</exception>
+        /// <remarks>
+        /// <para>
+        /// A GDI+ bitmap is useful to holding image data in memory, but it cannot be sent to the GPU for use as a texture. This method allows an application to convert the GDI+ bitmap into a 
+        /// <see cref="GorgonTexture2DView"/>. This view will be usable by pixel shaders.
+        /// </para>
+        /// <para>
+        /// The resulting <see cref="GorgonTexture2DView"/> will only contain a single mip level, and single array level. The only image type available will be 2D (i.e. image with a width and height). The GDI+ 
+        /// bitmap should have a 32bpp rgba format, or a 24bpp rgb format or else an exception will be thrown.
+        /// </para>
+        /// <para>
+        /// The optional <paramref name="options"/>parameter will define how Gorgon and shaders should handle the texture.  The <see cref="GorgonTextureLoadOptions"/> type contains the following:
+        /// <list type="bullet">
+        ///		<item>
+        ///			<term>Binding</term>
+        ///			<description>When defined, will indicate the <see cref="TextureBinding"/> that defines how the texture will be bound to the graphics pipeline. If it is omitted, then the binding will be 
+        ///         <see cref="TextureBinding.ShaderResource"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Usage</term>
+        ///			<description>When defined, will indicate the preferred usage for the texture. If it is omitted, then the usage will be set to <see cref="ResourceUsage.Default"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Multisample info</term>
+        ///			<description>When defined (i.e. not <b>null</b>), defines the multisampling to apply to the texture. If omitted, then the default is <see cref="GorgonMultisampleInfo.NoMultiSampling"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///		    <term>ConvertToPremultipliedAlpha</term>
+        ///		    <description>Converts the image to premultiplied alpha before uploading the image data to the texture.</description>
+        ///		</item>
+        /// </list>
+        /// </para>
+        /// </remarks>
+        public static GorgonTexture2DView ToTexture2DView(this Bitmap gdiBitmap, GorgonGraphics graphics, GorgonTexture2DLoadOptions options = null)
+        {
+            if (gdiBitmap is null)
+            {
+                throw new ArgumentNullException(nameof(gdiBitmap));
+            }
+
+            if (graphics is null)
+            {
+                throw new ArgumentNullException(nameof(graphics));
+            }
+
+            if (options is null)
+            {
+                options = _defaultLoadOptions;
+            }
+
+            if (string.IsNullOrEmpty(options.Name))
+            {
+                options.Name = GorgonGraphicsResource.GenerateName(GorgonTexture2D.NamePrefix);
+            }
+
+            using IGorgonImage image = gdiBitmap.ToGorgonImage();
+            if (options.ConvertToPremultipliedAlpha)
+            {
+                image.BeginUpdate()
+                     .ConvertToPremultipliedAlpha()
+                     .EndUpdate();
+            }
+
+            return GorgonTexture2DView.CreateTexture(graphics, new GorgonTexture2DInfo(image.Width, image.Height, image.Format)
+            {
+                Name = options.Name,
+                ArrayCount = 1,
+                MipLevels = 1,
+                Binding = TextureBinding.ShaderResource | options.Binding,                
+                Usage = options.Usage,
+                IsCubeMap = (options.IsTextureCube is null) ? image.ImageType == ImageType.ImageCube : options.IsTextureCube.Value,
+                MultisampleInfo = options.MultisampleInfo
+            }, image);
+        }
+
+        /// <summary>
+        /// Function to create a <see cref="GorgonTexture2DView"/> from a <see cref="GorgonImage"/>.
+        /// </summary>
+        /// <param name="image">The image used to create the texture.</param>
+        /// <param name="graphics">The graphics interface used to create the texture.</param>
+        /// <param name="options">[Optional] Options used to further define the texture.</param>
+        /// <returns>A new <see cref="GorgonTexture2DView"/> containing the data from the <paramref name="image"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="image"/>, or the <paramref name="graphics"/> parameter is <b>null</b>.</exception>
+        /// <remarks>
+        /// <para>
+        /// A <see cref="GorgonImage"/> is useful to holding image data in memory, but it cannot be sent to the GPU for use as a texture. This method allows an application to convert the 
+        /// <see cref="GorgonImage"/> into a <see cref="GorgonTexture2DView"/>. This view will be usable by pixel shaders.
+        /// </para>
+        /// <para>
+        /// If specified, the <paramref name="options"/>parameter will define how Gorgon and shaders should handle the texture.  The <see cref="GorgonTextureLoadOptions"/> type contains the following:
+        /// <list type="bullet">
+        ///		<item>
+        ///			<term>Binding</term>
+        ///			<description>When defined, will indicate the <see cref="TextureBinding"/> that defines how the texture will be bound to the graphics pipeline. If it is omitted, then the binding will be 
+        ///         <see cref="TextureBinding.ShaderResource"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Usage</term>
+        ///			<description>When defined, will indicate the preferred usage for the texture. If it is omitted, then the usage will be set to <see cref="ResourceUsage.Default"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Multisample info</term>
+        ///			<description>When defined (i.e. not <b>null</b>), defines the multisampling to apply to the texture. If omitted, then the default is <see cref="GorgonMultisampleInfo.NoMultiSampling"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///		    <term>ConvertToPremultipliedAlpha</term>
+        ///		    <description>Converts the image to premultiplied alpha before uploading the image data to the texture.</description>
+        ///		</item>
+        /// </list>
+        /// </para>
+        /// </remarks>
+        public static GorgonTexture2DView ToTexture2DView(this IGorgonImage image,
+                                                  GorgonGraphics graphics,
+                                                  GorgonTexture2DLoadOptions options = null)
+        {
+            if (image is null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            if (graphics is null)
+            {
+                throw new ArgumentNullException(nameof(graphics));
+            }
+
+            if (options is null)
+            {
+                options = _defaultLoadOptions;
+            }
+
+            if (string.IsNullOrEmpty(options.Name))
+            {
+                options.Name = GorgonGraphicsResource.GenerateName(GorgonTexture2D.NamePrefix);
+            }
+
+            if (options.ConvertToPremultipliedAlpha)
+            {
+                image.BeginUpdate()
+                     .ConvertToPremultipliedAlpha()
+                     .EndUpdate();
+            }
+
+            return GorgonTexture2DView.CreateTexture(graphics, new GorgonTexture2DInfo(image.Width, image.Height, image.Format)
+            {
+                Name = options.Name,
+                ArrayCount = image.ArrayCount,
+                MipLevels = image.MipCount,
+                Binding = TextureBinding.ShaderResource | options.Binding,
+                Usage = options.Usage,
+                IsCubeMap = (options.IsTextureCube is null) ? image.ImageType == ImageType.ImageCube : options.IsTextureCube.Value,
+                MultisampleInfo = options.MultisampleInfo
+            }, image);
+        }
+
+        /// <summary>
         /// Function to create a <see cref="GorgonTexture3D"/> from a <see cref="GorgonImage"/>.
         /// </summary>
         /// <param name="image">The image used to create the texture.</param>
@@ -182,6 +343,82 @@ namespace Gorgon.Graphics.Core
             }
 
             return new GorgonTexture3D(graphics, image, options);
+        }
+
+        /// <summary>
+        /// Function to create a <see cref="GorgonTexture3DView"/> from a <see cref="GorgonImage"/>.
+        /// </summary>
+        /// <param name="image">The image used to create the texture.</param>
+        /// <param name="graphics">The graphics interface used to create the texture.</param>
+        /// <param name="options">[Optional] Options used to further define the texture.</param>
+        /// <returns>A new <see cref="GorgonTexture3DView"/> containing the data from the <paramref name="image"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="image"/>, or the <paramref name="graphics"/> parameter is <b>null</b>.</exception>
+        /// <remarks>
+        /// <para>
+        /// A <see cref="GorgonImage"/> is useful to holding image data in memory, but it cannot be sent to the GPU for use as a texture. This method allows an application to convert the 
+        /// <see cref="GorgonImage"/> into a <see cref="GorgonTexture3DView"/>. This view will be usable by pixel shaders.
+        /// </para>
+        /// <para>
+        /// If specified, the <paramref name="options"/>parameter will define how Gorgon and shaders should handle the texture.  The <see cref="GorgonTextureLoadOptions"/> type contains the following:
+        /// <list type="bullet">
+        ///		<item>
+        ///			<term>Binding</term>
+        ///			<description>When defined, will indicate the <see cref="TextureBinding"/> that defines how the texture will be bound to the graphics pipeline. If it is omitted, then the binding will be 
+        ///         <see cref="TextureBinding.ShaderResource"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Usage</term>
+        ///			<description>When defined, will indicate the preferred usage for the texture. If it is omitted, then the usage will be set to <see cref="ResourceUsage.Default"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Multisample info</term>
+        ///			<description>This is not available on 3D textures, and is ignored.</description>
+        ///		</item>
+        ///		<item>
+        ///		    <term>ConvertToPremultipliedAlpha</term>
+        ///		    <description>Converts the image to premultiplied alpha before uploading the image data to the texture.</description>
+        ///		</item>
+        /// </list>
+        /// </para>
+        /// </remarks>
+        public static GorgonTexture3DView ToTexture3DView(this IGorgonImage image,
+                                                  GorgonGraphics graphics,
+                                                  GorgonTextureLoadOptions options = null)
+        {
+            if (image is null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            if (graphics is null)
+            {
+                throw new ArgumentNullException(nameof(graphics));
+            }
+
+            if (options is null)
+            {
+                options = _defaultLoadOptions;
+            }
+
+            if (string.IsNullOrEmpty(options.Name))
+            {
+                options.Name = GorgonGraphicsResource.GenerateName(GorgonTexture3D.NamePrefix);
+            }
+
+            if (options.ConvertToPremultipliedAlpha)
+            {
+                image.BeginUpdate()
+                     .ConvertToPremultipliedAlpha()
+                     .EndUpdate();
+            }
+
+            return GorgonTexture3DView.CreateTexture(graphics, new GorgonTexture3DInfo(image.Width, image.Height, image.Depth, image.Format)
+            {
+                Name = options.Name,
+                MipLevels = image.MipCount,
+                Binding = TextureBinding.ShaderResource | options.Binding,
+                Usage = options.Usage
+            }, image);
         }
 
         /// <summary>
@@ -322,6 +559,81 @@ namespace Gorgon.Graphics.Core
             }
 
             return new GorgonTexture1D(graphics, image, options);
+        }
+
+        /// <summary>
+        /// Function to create a <see cref="GorgonTexture1DView"/> from a <see cref="GorgonImage"/>.
+        /// </summary>
+        /// <param name="image">The image used to create the texture.</param>
+        /// <param name="graphics">The graphics interface used to create the texture.</param>
+        /// <param name="options">[Optional] Options used to further define the texture.</param>
+        /// <returns>A new <see cref="GorgonTexture1DView"/> containing the data from the <paramref name="image"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the <paramref name="image"/>, or the <paramref name="graphics"/> parameter is <b>null</b>.</exception>
+        /// <remarks>
+        /// <para>
+        /// A <see cref="GorgonImage"/> is useful to holding image data in memory, but it cannot be sent to the GPU for use as a texture. This method allows an application to convert the 
+        /// <see cref="GorgonImage"/> into a <see cref="GorgonTexture1DView"/>. 
+        /// </para>
+        /// <para>
+        /// If specified, the <paramref name="options"/>parameter will define how Gorgon and shaders should handle the texture.  The <see cref="GorgonTextureLoadOptions"/> type contains the following:
+        /// <list type="bullet">
+        ///		<item>
+        ///			<term>Binding</term>
+        ///			<description>When defined, will indicate the <see cref="TextureBinding"/> that defines how the texture will be bound to the graphics pipeline. If it is omitted, then the binding will be 
+        ///         <see cref="TextureBinding.ShaderResource"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Usage</term>
+        ///			<description>When defined, will indicate the preferred usage for the texture. If it is omitted, then the usage will be set to <see cref="ResourceUsage.Default"/>.</description>
+        ///		</item>
+        ///		<item>
+        ///			<term>Multisample info</term>
+        ///			<description>This is not available on 1D textures, and is ignored.</description>
+        ///		</item>
+        ///		<item>
+        ///		    <term>ConvertToPremultipliedAlpha</term>
+        ///		    <description>Converts the image to premultiplied alpha before uploading the image data to the texture.</description>
+        ///		</item>
+        /// </list>
+        /// </para>
+        /// </remarks>
+        public static GorgonTexture1DView ToTexture1DView(this IGorgonImage image, GorgonGraphics graphics, GorgonTextureLoadOptions options = null)
+        {
+            if (image is null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            if (graphics is null)
+            {
+                throw new ArgumentNullException(nameof(graphics));
+            }
+
+            if (options is null)
+            {
+                options = _defaultLoadOptions;
+            }
+
+            if (string.IsNullOrEmpty(options.Name))
+            {
+                options.Name = GorgonGraphicsResource.GenerateName(GorgonTexture1D.NamePrefix);
+            }
+
+            if (options.ConvertToPremultipliedAlpha)
+            {
+                image.BeginUpdate()
+                     .ConvertToPremultipliedAlpha()
+                     .EndUpdate();
+            }
+
+            return GorgonTexture1DView.CreateTexture(graphics, new GorgonTexture1DInfo(image.Width, image.Format)
+            {
+                Name = options.Name,
+                ArrayCount = image.ArrayCount,
+                MipLevels = image.MipCount,
+                Binding = TextureBinding.ShaderResource | options.Binding,
+                Usage = options.Usage
+            }, image);
         }
     }
 }
